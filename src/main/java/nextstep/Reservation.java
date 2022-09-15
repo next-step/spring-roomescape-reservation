e8cb2f1ad382ae08e0db7ad02dc88a74f9eecacd
boorownie <probitanima11@gmail.com>
2022-09-15 16:33:18 +0900

    default code

---
+++ src/main/java/nextstep/Reservation.java
@@ -0,0 +1,28 @@
+package nextstep;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+    private LocalDate date;
+    private LocalTime time;
+    private String name;
+
+    public Reservation(LocalDate date, LocalTime time, String name) {
+        this.date = date;
+        this.time = time;
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

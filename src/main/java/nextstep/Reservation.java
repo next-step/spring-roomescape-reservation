eb0bf04a126edf387dbcb210d69ec07013a418f0
Gomding <backchida@gmail.com>
2022-10-12 00:41:55 +0900

    [Step1] refactor: Reservation 객체에 id 값 추가 (객체 생성시 id값이 증가하도록 구현)

--- src/main/java/nextstep/Reservation.java
+++ src/main/java/nextstep/Reservation.java
@@ -2,16 +2,26 @@ package nextstep;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Reservation {
+
+    private static AtomicLong incrementCount = new AtomicLong();
+    private Long id;
+
     private LocalDate date;
     private LocalTime time;
     private String name;
 
     public Reservation(LocalDate date, LocalTime time, String name) {
+        this.id = incrementCount.incrementAndGet();
         this.date = date;
         this.time = time;
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public LocalDate getDate() {

854641b8bda74ea4f403464142dfd1d643830ddd
Gomding <backchida@gmail.com>
2022-10-12 00:24:08 +0900

    [Step1] feature: 방탈출 예약 요청을 받기위한 Controller 생성

---
+++ src/main/java/nextstep/ReservationController.java
@@ -0,0 +1,9 @@
+package nextstep;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+}

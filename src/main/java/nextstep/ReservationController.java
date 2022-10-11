bc8c7e6af4f14858f73e862a7e6d7b7a5580f16f
Gomding <backchida@gmail.com>
2022-10-12 00:55:24 +0900

    [Step1] feature: 예약 생성 요청을 처리할 수 있는 POST Mapping 추가

--- src/main/java/nextstep/ReservationController.java
+++ src/main/java/nextstep/ReservationController.java
@@ -1,9 +1,25 @@
 package nextstep;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
+
+    List<Reservation> reservations = new ArrayList<>();
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody ReservationCreateRequest request) {
+        Reservation reservation = request.toObject();
+        reservations.add(request.toObject());
+        return ResponseEntity.created(URI.create("/reservations/" + reservation.getId())).build();
+    }
 }

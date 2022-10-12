cbe0596529e163b614a8d9e6e572a5fd8722d4a8
Gomding <backchida@gmail.com>
2022-10-12 21:08:30 +0900

    [Step1] refactor: 사용하지 않는 import 정리

--- src/main/java/nextstep/ReservationController.java
+++ src/main/java/nextstep/ReservationController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;

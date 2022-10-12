badd297a6f529e01d095afff1464d520eac00db1
Gomding <backchida@gmail.com>
2022-10-12 21:26:53 +0900

    [Step1] feature: 예약 생성 시 해당 일시에 이미 예약이 존재하면 예외가 발생하도록 구현

--- src/main/java/nextstep/ReservationController.java
+++ src/main/java/nextstep/ReservationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -17,9 +18,17 @@ public class ReservationController {
 
     @PostMapping
     public ResponseEntity<Void> createReservation(@RequestBody ReservationCreateRequest request) {
+        if (existsReservation(request.getDate(), request.getTime())) {
+            throw new IllegalStateException("해당 일시에 이미 예약이 존재하여 예약이 불가능합니다.");
+        }
         Reservation reservation = request.toObject();
         reservations.add(reservation);
         return ResponseEntity.created(URI.create("/reservations/" + reservation.getId())).build();
+    }
+
+    private boolean existsReservation(LocalDate date, LocalTime time) {
+        return reservations.stream()
+                .anyMatch(it -> Objects.equals(it.getDate(), date) && Objects.equals(it.getTime(), time));
     }
 
     @GetMapping

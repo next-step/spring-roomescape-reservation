4a16afb4de647e0d0b9ff1f8af725ccbb8f74903
xrabcde <xrabcde@gmail.com>
2022-10-12 23:23:16 +0900

    test: 중복 예약인 경우 예외테스트 작성

--- src/test/java/nextstep/controller/ReservationControllerTest.java
+++ src/test/java/nextstep/controller/ReservationControllerTest.java
@@ -3,6 +3,7 @@ package nextstep.controller;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.dto.ErrorResponse;
 import nextstep.dto.ReservationCreateRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -35,6 +36,22 @@ class ReservationControllerTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    @DisplayName("POST 예약 생성 - 중복 예약 시, 예약 실패")
+    void failToCreate() {
+        // given
+        ReservationCreateRequest request = new ReservationCreateRequest("2022-12-05", "12:05", "조아라");
+        createReservation(request);
+
+        // when
+        ExtractableResponse<Response> response = createReservation(request);
+        ErrorResponse errorResponse = response.as(ErrorResponse.class);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(errorResponse.getMessage()).isEqualTo("동시간대에 이미 예약이 존재합니다.");
     }
 
     private ExtractableResponse<Response> createReservation(ReservationCreateRequest request) {

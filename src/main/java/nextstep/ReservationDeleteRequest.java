ac25406f91b13c2a8542e6a6eabe56a4a85bb4ce
Gomding <backchida@gmail.com>
2022-10-12 21:06:48 +0900

    [Step1] feature: 예약 삭제 요청 클래스 생성

---
+++ src/main/java/nextstep/ReservationDeleteRequest.java
@@ -0,0 +1,26 @@
+package nextstep;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ReservationDeleteRequest {
+    private final LocalDate date;
+    private final LocalTime time;
+
+    private ReservationDeleteRequest(LocalDate date, LocalTime time) {
+        this.date = date;
+        this.time = time;
+    }
+
+    public static ReservationDeleteRequest of(String date, String time) {
+        return new ReservationDeleteRequest(LocalDate.parse(date), LocalTime.parse(time + ":00"));
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}

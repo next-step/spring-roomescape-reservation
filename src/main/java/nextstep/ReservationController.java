1dfac98750364ac5489caac4d491199d5f3014f1
Gomding <backchida@gmail.com>
2022-10-12 21:57:08 +0900

    [Step1] refactor: 예약을 담고, 저장 삭제 조회를 담당하는 데이터베이스 생성

--- src/main/java/nextstep/ReservationController.java
+++ src/main/java/nextstep/ReservationController.java
@@ -5,36 +5,27 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
 
-    List<Reservation> reservations = new ArrayList<>();
+    private final ReservationDatabase reservations = new ReservationDatabase();
 
     @PostMapping
     public ResponseEntity<Void> createReservation(@RequestBody ReservationCreateRequest request) {
-        if (existsReservation(request.getDate(), request.getTime())) {
+        if (reservations.existsReservation(request.getDate(), request.getTime())) {
             throw new IllegalStateException("해당 일시에 이미 예약이 존재하여 예약이 불가능합니다.");
         }
         Reservation reservation = request.toObject();
-        reservations.add(reservation);
+        reservations.save(reservation);
         return ResponseEntity.created(URI.create("/reservations/" + reservation.getId())).build();
-    }
-
-    private boolean existsReservation(LocalDate date, LocalTime time) {
-        return reservations.stream()
-                .anyMatch(it -> Objects.equals(it.getDate(), date) && Objects.equals(it.getTime(), time));
     }
 
     @GetMapping
     public ResponseEntity<List<ReservationResponse>> getReservation(@RequestParam("date") String date) {
-        List<ReservationResponse> findReservations = reservations.stream()
-                .filter(it -> Objects.equals(it.getDate(), LocalDate.parse(date)))
+        List<ReservationResponse> findReservations = reservations.findByDate(LocalDate.parse(date)).stream()
                 .map(it -> new ReservationResponse(it.getId(), it.getDate(), it.getTime(), it.getName()))
                 .toList();
         return ResponseEntity.ok(findReservations);
@@ -43,10 +34,7 @@ public class ReservationController {
     @DeleteMapping
     public ResponseEntity<Void> deleteReservation(@RequestParam("date") String date, @RequestParam("time") String time) {
         ReservationDeleteRequest request = ReservationDeleteRequest.of(date, time);
-        reservations.stream()
-                .filter(it -> Objects.equals(it.getDate(), request.getDate()) && Objects.equals(it.getTime(), request.getTime()))
-                .findFirst()
-                .ifPresent(reservations::remove);
+        reservations.deleteByDateAndTime(request.getDate(), request.getTime());
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/nextstep/ReservationDatabase.java
@@ -0,0 +1,43 @@
+package nextstep;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class ReservationDatabase {
+
+    public final List<Reservation> reservations = new ArrayList<>();
+
+    public Reservation save(Reservation reservation) {
+        reservations.add(reservation);
+        return reservation;
+    }
+
+    public boolean existsReservation(LocalDate date, LocalTime time) {
+        return this.findByDateAndTime(date, time).isPresent();
+    }
+
+    public Optional<Reservation> findByDateAndTime(LocalDate date, LocalTime time) {
+        return reservations.stream()
+                .filter(it -> Objects.equals(it.getDate(), date) && Objects.equals(it.getTime(), time))
+                .findAny();
+    }
+
+    public List<Reservation> findByDate(LocalDate date) {
+        return reservations.stream()
+                .filter(it -> Objects.equals(it.getDate(), date))
+                .toList();
+    }
+
+    public boolean deleteByDateAndTime(LocalDate date, LocalTime time) {
+        Optional<Reservation> findReservation = findByDateAndTime(date, time);
+        if (findReservation.isPresent()) {
+            reservations.remove(findReservation.get());
+            return true;
+        }
+        return false;
+    }
+}

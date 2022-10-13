e8368848bc018e064d61e9001c5f88f3e11dd1ec
Hyeon9mak <jinha3507@gmail.com>
2022-10-13 10:48:18 +0900

    refactor: 예약 서비스 계층 분리, 붎필요한 일급 컬렉션 제거

---
+++ src/main/java/nextstep/application/ReservationService.java
@@ -0,0 +1,46 @@
+package nextstep.application;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import nextstep.domain.Reservation;
+import nextstep.domain.ReservationRepository;
+import nextstep.ui.request.ReservationCreateRequest;
+import nextstep.ui.response.ReservationResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    public ReservationResponse create(ReservationCreateRequest request) {
+        validateDuplicateDateTime(request.getDate(), request.getTime());
+
+        Reservation reservation = new Reservation(
+            request.getDate(),
+            request.getTime(),
+            request.getName()
+        );
+
+        return ReservationResponse.from(reservationRepository.save(reservation));
+    }
+
+    private void validateDuplicateDateTime(LocalDate date, LocalTime time) {
+        if (reservationRepository.existsByDateTime(date, time)) {
+            throw new IllegalArgumentException("날짜와 시간이 똑같은 예약이 이미 존재합니다.");
+        }
+    }
+
+    public List<ReservationResponse> findAllByDate(LocalDate date) {
+        return ReservationResponse.of(reservationRepository.findAllByDate(date));
+    }
+
+    public void removeByDateTime(LocalDate date, LocalTime time) {
+        reservationRepository.deleteByDateTime(date, time);
+    }
+}

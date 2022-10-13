8a8ccf7abe59849c6ff88643c28691bb8c40aa67
Gomding <backchida@gmail.com>
2022-10-14 00:37:36 +0900

    [Step2] feature: ReservationJdbcRepository에 save 추가

---
+++ src/main/java/nextstep/ReservationJdbcRepository.java
@@ -0,0 +1,63 @@
+package nextstep;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Primary
+public class ReservationJdbcRepository implements ReservationRepository{
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    public ReservationJdbcRepository(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("reservation")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    @Override
+    public Reservation save(Reservation reservation) {
+        SqlParameterSource source = new MapSqlParameterSource()
+                .addValue("date", reservation.getDate())
+                .addValue("time", reservation.getTime())
+                .addValue("name", reservation.getName());
+        reservation.setId((Long) simpleJdbcInsert.executeAndReturnKey(source));
+        return reservation;
+    }
+
+    @Override
+    public boolean existsReservation(LocalDate date, LocalTime time) {
+        return false;
+    }
+
+    @Override
+    public Optional<Reservation> findByDateAndTime(LocalDate date, LocalTime time) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Reservation> findByDate(LocalDate date) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteByDateAndTime(LocalDate date, LocalTime time) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}

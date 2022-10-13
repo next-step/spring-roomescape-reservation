665d26e07cccd69e2fae6b95762cae123971e1cd
Hyeon9mak <jinha3507@gmail.com>
2022-10-13 10:11:46 +0900

    feat: 날짜별 조회 관련 Dao 구현

--- src/test/java/nextstep/domain/ReservationDaoTest.java
+++ src/test/java/nextstep/domain/ReservationDaoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,18 +34,46 @@ class ReservationDaoTest {
 
     @DisplayName("예약 저장")
     @Test
-    void insert() {
+    void save() {
         Reservation reservation = new Reservation(
             LocalDate.of(2022, 10, 1),
             LocalTime.of(2, 0),
             "최현구"
         );
 
-        Reservation saved = reservationDao.insert(reservation);
+        Reservation saved = reservationDao.save(reservation);
 
         assertThat(saved.getId()).isNotNull();
         assertThat(saved).usingRecursiveComparison()
             .ignoringFields("id")
             .isEqualTo(reservation);
+    }
+
+    @DisplayName("날짜에 해당하는 모든 예약 조회")
+    @Test
+    void findAllByDate() {
+        Reservation 최현구_10월1일_예약 = saveReservation(
+            LocalDate.of(2022, 10, 1),
+            LocalTime.of(1, 0),
+            "최현구"
+        );
+        Reservation 최현팔_10월1일_예약 = saveReservation(
+            LocalDate.of(2022, 10, 1),
+            LocalTime.of(5, 0),
+            "최현팔"
+        );
+        Reservation 최현칠_9월5일_예약 = saveReservation(
+            LocalDate.of(2022, 9, 5),
+            LocalTime.of(1, 0),
+            "최현칠"
+        );
+
+        List<Reservation> reservations = reservationDao.findAllByDate(LocalDate.of(2022, 10, 1));
+
+        assertThat(reservations).containsExactly(최현구_10월1일_예약, 최현팔_10월1일_예약);
+    }
+
+    private Reservation saveReservation(LocalDate date, LocalTime time, String name) {
+        return reservationDao.save(new Reservation(date, time, name));
     }
 }

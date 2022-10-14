a1272d5d80071ea8ca15a2d254caefb2f02a243d
Gomding <backchida@gmail.com>
2022-10-14 18:55:43 +0900

    [Step3] feature: 테마 생성 요청 테스트 작성

--- src/main/java/nextstep/theme/ThemeController.java
+++ src/main/java/nextstep/theme/ThemeController.java
@@ -1,11 +1,19 @@
 package nextstep.theme;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/themes")
 public class ThemeController {
 
-
+    @PostMapping
+    public ResponseEntity<Void> createTheme(@RequestBody ThemeCreateRequest request) {
+        return ResponseEntity.created(URI.create("/themes/1")).build();
+    }
 }
---
+++ src/test/java/nextstep/theme/ThemeControllerTest.java
@@ -0,0 +1,47 @@
+package nextstep.theme;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.SpringControllerTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ThemeControllerTest extends SpringControllerTest {
+
+    @Autowired
+    private ThemeJdbcRepository themeJdbcRepository;
+
+    @Override
+    @BeforeEach
+    protected void setUp() {
+        super.setUp();
+        themeJdbcRepository.clear();
+    }
+
+    @DisplayName("예약 생성")
+    @Test
+    void createTheme() {
+        // given
+        ThemeCreateRequest request = new ThemeCreateRequest("404호의 비밀", "분명 있어야할 곳에 아무것도 없었다.. 남겨진 쪽지엔 404 NOT FOUND 만이 남아있는데...", 22000);
+
+        // when
+        ExtractableResponse<Response> response = 테마_생성_요청(request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private static ExtractableResponse<Response> 테마_생성_요청(ThemeCreateRequest request) {
+        return given()
+                .body(request).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/themes")
+                .then().log().all().extract();
+    }
+}

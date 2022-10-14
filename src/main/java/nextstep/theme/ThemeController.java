0309d2625682a55e04b988ba9e1a9ae347268970
Gomding <backchida@gmail.com>
2022-10-14 18:34:56 +0900

    [Step3] feature: 테마 관련 요청을 처리할 ThemeController 생성

---
+++ src/main/java/nextstep/theme/ThemeController.java
@@ -0,0 +1,11 @@
+package nextstep.theme;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/themes")
+public class ThemeController {
+
+
+}

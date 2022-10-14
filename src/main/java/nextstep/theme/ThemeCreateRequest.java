4e38bcb103706ce7d7fad026ede88f4d7c8b2fd0
Gomding <backchida@gmail.com>
2022-10-14 18:35:11 +0900

    [Step3] feature: 테마 생성 요청 클래스 생성

---
+++ src/main/java/nextstep/theme/ThemeCreateRequest.java
@@ -0,0 +1,25 @@
+package nextstep.theme;
+
+public class ThemeCreateRequest {
+    private final String name;
+    private final String desc;
+    private final int price;
+
+    public ThemeCreateRequest(String name, String desc, int price) {
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}

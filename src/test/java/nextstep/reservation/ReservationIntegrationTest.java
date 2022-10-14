fc979e6bb7a5bf46b3cc778a79860a9d0dd4e63f
ggyool <ggyool@naver.com>
2022-10-14 23:06:01 +0900

    [step1] 테스트 잘못 구현된 부분 수정

--- src/test/java/nextstep/reservation/ReservationIntegrationTest.java
+++ src/test/java/nextstep/reservation/ReservationIntegrationTest.java
@@ -1,37 +1,42 @@
 package nextstep.reservation;
 
 import static org.hamcrest.Matchers.startsWith;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import nextstep.reservation.web.ReservationController;
 import nextstep.reservation.web.request.MakeReservationRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ReservationIntegrationTest {
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private WebApplicationContext webApplicationContext;
 
     @Autowired
-    private ReservationController reservationController;
+    private ObjectMapper objectMapper;
 
     private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(reservationController)
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .addFilters(new CharacterEncodingFilter("UTF-8", true))
+            .alwaysDo(print())
             .build();
     }
 
@@ -44,7 +49,9 @@ class ReservationIntegrationTest {
         );
 
         mockMvc.perform(
-                post("/reservations").content(requestBody)
+                post("/reservations")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody)
             )
             .andExpect(status().isCreated())
             // TODO ggyool 추후 id 받아서 추가해야함
@@ -62,9 +69,9 @@ class ReservationIntegrationTest {
     @Test
     void cancelReservation() throws Exception {
         mockMvc.perform(
-                get("/reservations")
+                delete("/reservations")
                     .param("date", "2022-08-11")
-                    .param("time", "`13:00`")
+                    .param("time", "13:00")
             )
             .andExpect(status().isNoContent());
     }

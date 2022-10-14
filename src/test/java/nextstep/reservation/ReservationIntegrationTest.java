11bd5ef1c2d1b0505e0a5af163fd72999ac32d2c
ggyool <ggyool@naver.com>
2022-10-14 22:19:29 +0900

    [step1] web 레이어 관련 테스트 추가

--- src/test/java/nextstep/reservation/ReservationIntegrationTest.java
+++ src/test/java/nextstep/reservation/ReservationIntegrationTest.java
@@ -1,38 +1,71 @@
 package nextstep.reservation;
 
+import static org.hamcrest.Matchers.startsWith;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.reservation.web.ReservationController;
+import nextstep.reservation.web.request.MakeReservationRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
-@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ReservationIntegrationTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ReservationController reservationController;
 
     private MockMvc mockMvc;
 
-
     @BeforeEach
     void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(ReservationController.class)
+        mockMvc = MockMvcBuilders.standaloneSetup(reservationController)
             .build();
     }
 
     @Test
-    void makeReservation() {
+    void makeReservation() throws Exception {
+        String requestBody = objectMapper.writeValueAsString(
+            new MakeReservationRequest(
+                "2022-08-11", "13:00", "name"
+            )
+        );
 
+        mockMvc.perform(
+                post("/reservations").content(requestBody)
+            )
+            .andExpect(status().isCreated())
+            // TODO ggyool 추후 id 받아서 추가해야함
+            .andExpect(header().string("Location", startsWith("/reservations/")));
     }
 
     @Test
-    void listReservations() {
-
+    void listReservations() throws Exception {
+        mockMvc.perform(
+                get("/reservations").param("date", "2022-08-11")
+            )
+            .andExpect(status().isOk());
     }
 
     @Test
-    void cancelReservation() {
-
+    void cancelReservation() throws Exception {
+        mockMvc.perform(
+                get("/reservations")
+                    .param("date", "2022-08-11")
+                    .param("time", "`13:00`")
+            )
+            .andExpect(status().isNoContent());
     }
 }

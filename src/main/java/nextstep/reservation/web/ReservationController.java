04fb5e469240d5926fc489b4d442bbbedd2b7099
ggyool <ggyool@naver.com>
2022-10-14 23:09:35 +0900

    [step1] ReservationController web 관련 부분 구현

--- src/main/java/nextstep/reservation/web/ReservationController.java
+++ src/main/java/nextstep/reservation/web/ReservationController.java
@@ -1,11 +1,17 @@
 package nextstep.reservation.web;
 
+import java.net.URI;
 import java.util.List;
 import nextstep.reservation.domain.Reservation;
+import nextstep.reservation.web.request.CancelReservationRequest;
+import nextstep.reservation.web.request.ListReservationRequest;
+import nextstep.reservation.web.request.MakeReservationRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationController {
 
     @PostMapping
-    ResponseEntity<Void> makeReservation() {
-        return null;
+    ResponseEntity<Void> makeReservation(@RequestBody MakeReservationRequest requestBody) {
+        // TODO ggyool 아이디 받아오면 수정해야함
+        URI locationUri = URI.create("/reservations/1");
+        return ResponseEntity.created(locationUri)
+            .build();
     }
 
     @GetMapping
-    ResponseEntity<List<Reservation>> listReservations() {
+    ResponseEntity<List<Reservation>> listReservations(@ModelAttribute ListReservationRequest requestParams) {
         return null;
     }
 
     @DeleteMapping
-    ResponseEntity<Void> cancelReservation() {
-        return null;
+    ResponseEntity<Void> cancelReservation(@ModelAttribute CancelReservationRequest requestParams) {
+        return ResponseEntity.noContent()
+            .build();
     }
 }

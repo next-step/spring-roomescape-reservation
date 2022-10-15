68996a935ea2ae6c29cf088f62550e7c75fa9f89
Daeun Lee <leedaeun.dev@gmail.com>
2022-10-15 14:13:05 +0900

    fix: @RequestBody 추가

--- src/main/java/nextstep/controller/ReservationController.java
+++ src/main/java/nextstep/controller/ReservationController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class ReservationController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> make(ReservationRequest request) {
+    public ResponseEntity<Void> make(@RequestBody ReservationRequest request) {
         Integer reservationId = reservationService.make(request);
 
         return ResponseEntity.created(URI.create("/reservations/" + reservationId)).build();

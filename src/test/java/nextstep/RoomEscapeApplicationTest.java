4247bf498835bda67ebd8957843212f8cb5bf503
성시형 <wedge@woowahan.com>
2022-10-15 13:49:26 +0900

    [1단계] feat: 스프링 어플리케이션 생성

---
+++ src/test/java/nextstep/RoomEscapeApplicationTest.java
@@ -0,0 +1,17 @@
+package nextstep;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomEscapeApplicationTest {
+
+  @DisplayName("스프링 콘텍스트 로딩이 잘 이루어지는지 확인")
+  @Test
+  void runningTest() {
+    // do nothing
+  }
+}

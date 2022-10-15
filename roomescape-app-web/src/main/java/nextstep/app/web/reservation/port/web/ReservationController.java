4de31d1cef2fc098157d22db4e1ef35ba634eba9
김경준 <kouz@woowahan.com>
2022-10-15 23:34:48 +0900

    feat: 예약 생성 (Test Pass)

--- roomescape-app-web/src/main/java/nextstep/app/web/reservation/port/web/ReservationController.java
+++ roomescape-app-web/src/main/java/nextstep/app/web/reservation/port/web/ReservationController.java
@@ -1,8 +1,16 @@
 package nextstep.app.web.reservation.port.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class ReservationController {
-
+    @PostMapping("/reservations")
+    public ResponseEntity<Void> create(@RequestBody ReservationCreateRequest request) {
+        return ResponseEntity.created(URI.create("/reservations/1")).build();
+    }
 }

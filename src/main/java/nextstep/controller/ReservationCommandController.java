d27a26655f58cc47a52cb5466f3d00104b27ff42
성시형 <wedge@woowahan.com>
2022-10-16 12:20:55 +0900

    [1단계] feat : 예약 목록 조회 컨트롤러 구현

--- src/main/java/nextstep/controller/ReservationCommandController.java
+++ src/main/java/nextstep/controller/ReservationCommandController.java
@@ -4,7 +4,7 @@ import javax.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import nextstep.controller.dto.ReservationViewDto;
+import nextstep.controller.dto.ReservationViewRequest;
 import nextstep.domain.reservation.ReservationCommander;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/reservation")
+@RequestMapping("/reservations")
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class ReservationCommandController {
@@ -22,13 +22,13 @@ public class ReservationCommandController {
   ReservationCommander commander;
 
   @PostMapping
-  public ResponseEntity<Void> createReservation(@Valid @RequestBody ReservationViewDto.Create createReq){
+  public ResponseEntity<Void> createReservation(@Valid @RequestBody ReservationViewRequest.Create createReq){
     commander.createReservation(createReq.toDomainCommand());
     return ResponseEntity.ok().build();
   }
 
   @DeleteMapping
-  public ResponseEntity<Void> deleteReservation(@Valid @RequestBody ReservationViewDto.Delete deleteReq){
+  public ResponseEntity<Void> deleteReservation(@Valid @RequestBody ReservationViewRequest.Delete deleteReq){
     commander.deleteReservation(deleteReq.toDomainCommand());
     return ResponseEntity.ok().build();
   }

6fe1421f15012e30e4f5e939ea631c4fd3268a3d
livenow14 <xowjd41@naver.com>
2022-10-16 21:30:24 +0900

    refactor: 기존 콘솔 애플리케이션 리팩터링

--- src/main/java/nextstep/Main.java
+++ src/main/java/nextstep/Main.java
@@ -1,93 +1,89 @@
 package nextstep;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import nextstep.application.RoomEscapeService;
 import nextstep.application.dto.Reservation;
+import nextstep.application.dto.ReservationCreateReq;
+import nextstep.domain.repository.ReservationRepository;
 
 public class Main {
-    private static final String INPUT_1 = "1";
-    private static final String INPUT_2 = "2";
-    private static final String INPUT_3 = "3";
-    private static final String INPUT_4 = "4";
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<Reservation> reservations = new ArrayList<>();
+  private static final String INPUT_1 = "1";
+  private static final String INPUT_2 = "2";
+  private static final String INPUT_3 = "3";
+  private static final String INPUT_4 = "4";
 
-        while (true) {
-            System.out.println("메뉴를 선택하세요.");
-            System.out.println("1: 예약");
-            System.out.println("2: 예약 취소");
-            System.out.println("3: 예약 조회");
-            System.out.println("4: 종료");
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    RoomEscapeService service = new RoomEscapeService(new ReservationRepository());
 
-            String menuInput = scanner.nextLine();
-            if (INPUT_1.equals(menuInput)) {
+    while (true) {
+      System.out.println("메뉴를 선택하세요.");
+      System.out.println("1: 예약");
+      System.out.println("2: 예약 취소");
+      System.out.println("3: 예약 조회");
+      System.out.println("4: 종료");
 
-                System.out.println("예약 정보를 입력하세요.");
-                System.out.println();
+      String menuInput = scanner.nextLine();
+      if (INPUT_1.equals(menuInput)) {
 
-                System.out.println("날짜 (ex.2022-08-11)");
-                String date = scanner.nextLine();
+        System.out.println("예약 정보를 입력하세요.");
+        System.out.println();
 
-                System.out.println("시간 (ex.13:00)");
-                String time = scanner.nextLine();
+        System.out.println("날짜 (ex.2022-08-11)");
+        String date = scanner.nextLine();
 
-                System.out.println("예약자 이름");
-                String name = scanner.nextLine();
+        System.out.println("시간 (ex.13:00)");
+        String time = scanner.nextLine();
 
-                Reservation reservation = new Reservation(
-                        LocalDate.parse(date),
-                        LocalTime.parse(time + ":00"),
-                        name
-                );
+        System.out.println("예약자 이름");
+        String name = scanner.nextLine();
 
-                reservations.add(reservation);
-                System.out.println("예약이 등록되었습니다.");
-            }
+        ReservationCreateReq reservation = ReservationCreateReq.builder()
+            .date(LocalDate.parse(date))
+            .time(time)
+            .name(name)
+            .build();
 
-            if (INPUT_2.equals(menuInput)) {
+        service.create(reservation);
+        System.out.println("예약이 등록되었습니다.");
+      }
 
-                System.out.println("취소할 예약 정보를 입력하세요.");
-                System.out.println();
+      if (INPUT_2.equals(menuInput)) {
 
-                System.out.println("날짜 (ex.2022-08-11)");
-                String date = scanner.nextLine();
+        System.out.println("취소할 예약 정보를 입력하세요.");
+        System.out.println();
 
-                System.out.println("시간 (ex.13:00)");
-                String time = scanner.nextLine();
+        System.out.println("날짜 (ex.2022-08-11)");
+        String date = scanner.nextLine();
 
-                reservations.stream()
-                        .filter(it -> Objects.equals(it.getDate(), LocalDate.parse(date)) && Objects.equals(it.getTime(), LocalDate.parse(time)))
-                        .findFirst()
-                        .ifPresent(reservations::remove);
+        System.out.println("시간 (ex.13:00)");
+        String time = scanner.nextLine();
 
-                System.out.println("예약이 취소되었습니다.");
-            }
+        service.removeReservation(LocalDate.parse(date), time);
+        System.out.println("예약이 취소되었습니다.");
+      }
 
-            if (INPUT_3.equals(menuInput)) {
+      if (INPUT_3.equals(menuInput)) {
 
-                System.out.println("예약 조회 할 날짜를 입력하세요.");
-                System.out.println();
+        System.out.println("예약 조회 할 날짜를 입력하세요.");
+        System.out.println();
 
-                System.out.println("날짜 (ex.2022-08-11)");
-                String date = scanner.nextLine();
+        System.out.println("날짜 (ex.2022-08-11)");
+        String date = scanner.nextLine();
 
-                reservations.stream()
-                        .filter(it -> it.getDate().isEqual(LocalDate.parse(date)))
-                        .collect(Collectors.toList())
-                        .forEach(System.out::println);
-            }
+        service.findReservations(LocalDate.parse(date));
+      }
 
-            if (INPUT_4.equals(menuInput)) {
+      if (INPUT_4.equals(menuInput)) {
 
-                break;
-            }
-        }
+        break;
+      }
     }
+  }
 }

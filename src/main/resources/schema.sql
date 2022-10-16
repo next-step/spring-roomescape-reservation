b29673653f073f282abf74e9ff8280cf94b51d18
Gomding <backchida@gmail.com>
2022-10-16 23:18:58 +0900

    [Step3] feature: scheme.sql 에 schedule 테이블 생성문 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,9 +7,17 @@ CREATE TABLE reservation(
                 );
 
 CREATE TABLE theme(
-                            id IDENTITY NOT NULL,
-                            name VARCHAR(100),
-                            desc VARCHAR(500),
-                            price INTEGER,
-                            PRIMARY KEY (id)
+               id IDENTITY NOT NULL,
+               name VARCHAR(100),
+               desc VARCHAR(500),
+               price INTEGER,
+               PRIMARY KEY (id)
+);
+
+CREATE TABLE schedule(
+               id IDENTITY NOT NULL,
+               theme_id INTEGER,
+               date DATE,
+               time TIME,
+               PRIMARY KEY (id)
 );

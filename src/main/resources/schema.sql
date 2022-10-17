34cd91839f1863e55fc0a204e65a424cd4ad6378
ggyool <ggyool@naver.com>
2022-10-17 11:55:48 +0900

    [step3] scheme.sql 탭 간격 정리

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,25 +2,25 @@ DROP TABLE IF EXISTS theme, schedule, reservation;
 
 CREATE TABLE theme
 (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    desc VARCHAR(255) NOT NULL,
-    price BIGINT NOT NULL
+    id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name  VARCHAR(255) NOT NULL,
+    desc  VARCHAR(255) NOT NULL,
+    price BIGINT       NOT NULL
 );
 
 CREATE TABLE schedule
 (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
     theme_id BIGINT NOT NULL,
-    date DATE NOT NULL,
-    time TIME NOT NULL
+    date     DATE   NOT NULL,
+    time     TIME   NOT NULL
 );
 
 CREATE TABLE reservation
 (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    schedule_id BIGINT NOT NULL,
-    date DATE NOT NULL,
-    time TIME NOT NULL,
-    name VARCHAR(255) NOT NULL
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    schedule_id BIGINT       NOT NULL,
+    date        DATE         NOT NULL,
+    time        TIME         NOT NULL,
+    name        VARCHAR(255) NOT NULL
 );

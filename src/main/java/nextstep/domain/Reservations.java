e3d23ab088c28044770340e53e1475b9b579f0da
Jang Jaeju <44603719+jjj0611@users.noreply.github.com>
2022-10-17 19:24:52 +0900

    2단계 - 데이터베이스 적용 (#33)
    * add jdbc & h2 dependencies to apply runtime database
    
    * apply database
    
    Co-authored-by: Jaeju Jang <jaeju.island@woowahan.com>

--- src/main/java/nextstep/domain/Reservations.java
+++ src/main/java/nextstep/domain/Reservations.java
@@ -1,39 +1,43 @@
 package nextstep.domain;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
+@Component
 public class Reservations {
-    private final List<Reservation> reservations;
+    private static final RowMapper<Reservation> RESERVATION_ROW_MAPPER = (resultSet, rowNum) -> new Reservation(
+        resultSet.getDate("reservation_date").toLocalDate(),
+        resultSet.getTime("reservation_time").toLocalTime(),
+        resultSet.getString("name")
+    );
 
-    public Reservations() {
+    private final List<Reservation> reservations;
+    private final JdbcTemplate jdbcTemplate;
+
+    public Reservations(JdbcTemplate jdbcTemplate) {
         this.reservations = new ArrayList<>();
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public void add(Reservation reservation) {
-        if (isAlreadyBooked(reservation.getReservationTime())) {
-            throw new IllegalArgumentException("이미 예약되어 있는 시간입니다.");
+        try {
+            jdbcTemplate.update("INSERT INTO reservation (name, reservation_date, reservation_time) VALUES (?, ?, ?)", reservation.getName(), reservation.getDate(), reservation.getTime());
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalArgumentException("예약할 수 없습니다.");
         }
-        reservations.add(reservation);
     }
 
     public void remove(ReservationTime reservationTime) {
-        reservations.stream()
-            .filter(it -> Objects.equals(it.getReservationTime(), reservationTime))
-            .findFirst()
-            .ifPresent(reservations::remove);
+        jdbcTemplate.update("DELETE reservation WHERE reservation_date = ? AND reservation_time = ?", reservationTime.getDate(), reservationTime.getTime());
     }
 
     public List<Reservation> findBy(LocalDate targetDate) {
-        return reservations.stream()
-            .filter(it -> Objects.equals(it.getDate(), targetDate))
-            .toList();
-    }
-
-    private boolean isAlreadyBooked(ReservationTime reservationTime) {
-        return reservations.stream()
-            .anyMatch(it -> Objects.equals(it.getReservationTime(), reservationTime));
+        return jdbcTemplate.query("SELECT * FROM reservation WHERE reservation_date = ?", RESERVATION_ROW_MAPPER, targetDate);
     }
 }

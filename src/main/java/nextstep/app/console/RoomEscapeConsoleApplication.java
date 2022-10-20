332c159a3b1b8830d2d67dcfb972c6e18e20cedb
남윤서 <devysnam@gmail.com>
2022-10-20 22:25:32 +0900

    feat: apply DI in console application

--- src/main/java/nextstep/app/console/RoomEscapeConsoleApplication.java
+++ src/main/java/nextstep/app/console/RoomEscapeConsoleApplication.java
@@ -1,85 +1,35 @@
 package nextstep.app.console;
 
-import nextstep.core.Reservation;
 import nextstep.core.ReservationRepository;
-import nextstep.infra.noop.ReservationInMemoryRepository;
+import nextstep.infra.h2.ReservationH2Repository;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import javax.sql.DataSource;
 import java.util.Scanner;
 
 public class RoomEscapeConsoleApplication {
-    private static final String INPUT_1 = "1";
-    private static final String INPUT_2 = "2";
-    private static final String INPUT_3 = "3";
-    private static final String INPUT_4 = "4";
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ReservationRepository repository = new ReservationInMemoryRepository();
+        DataSource dataSource = DataSourceBuilder.create()
+                .driverClassName("org.h2.Driver")
+                .url("jdbc:h2:mem:room-escape;MODE=MySQL")
+                .username("sa")
+                .build();
+        JdbcTemplate template = new JdbcTemplate(dataSource);
+        ReservationRepository repository = new ReservationH2Repository(template);
 
-        while (true) {
-            System.out.println("메뉴를 선택하세요.");
-            System.out.println("1: 예약");
-            System.out.println("2: 예약 취소");
-            System.out.println("3: 예약 조회");
-            System.out.println("4: 종료");
-
-            String menuInput = scanner.nextLine();
-            if (INPUT_1.equals(menuInput)) {
-
-                System.out.println("예약 정보를 입력하세요.");
-                System.out.println();
-
-                System.out.println("날짜 (ex.2022-08-11)");
-                String date = scanner.nextLine();
-
-                System.out.println("시간 (ex.13:00)");
-                String time = scanner.nextLine();
-
-                System.out.println("예약자 이름");
-                String name = scanner.nextLine();
-
-                Reservation reservation = new Reservation(
-                        LocalDate.parse(date),
-                        LocalTime.parse(time + ":00"),
-                        name
+        template.execute("DROP TABLE IF EXISTS reservation");
+        template.execute("""
+                create table reservation
+                (
+                    id   bigint      not null primary key auto_increment comment '예약 아이디',
+                    date date        not null default now() comment '예약 날짜',
+                    time time        not null default now() comment '예약 시간',
+                    name varchar(32) not null comment '예약자 이름'
                 );
+                                """);
 
-                repository.save(reservation);
-                System.out.println("예약이 등록되었습니다.");
-            }
-
-            if (INPUT_2.equals(menuInput)) {
-
-                System.out.println("취소할 예약 정보를 입력하세요.");
-                System.out.println();
-
-                System.out.println("날짜 (ex.2022-08-11)");
-                String date = scanner.nextLine();
-
-                System.out.println("시간 (ex.13:00)");
-                String time = scanner.nextLine();
-
-                repository.deleteByDateAndTime(LocalDate.parse(date), LocalTime.parse(time));
-                System.out.println("예약이 취소되었습니다.");
-            }
-
-            if (INPUT_3.equals(menuInput)) {
-
-                System.out.println("예약 조회 할 날짜를 입력하세요.");
-                System.out.println();
-
-                System.out.println("날짜 (ex.2022-08-11)");
-                String date = scanner.nextLine();
-
-                repository.findAllByDate(LocalDate.parse(date))
-                        .forEach(System.out::println);
-            }
-
-            if (INPUT_4.equals(menuInput)) {
-                break;
-            }
-        }
+        Scanner scanner = new Scanner(System.in);
+        new ReservationController(scanner, repository).run();
     }
 }

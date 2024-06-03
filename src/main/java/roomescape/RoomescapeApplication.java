0599319d058cee33b27556c9eb11572a986940ba
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2024-06-03 22:02:59 +0900

    test: step 0 push test

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RoomescapeApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
     }

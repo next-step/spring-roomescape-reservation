7fe91e6d91801562e515696cdad34ddfae94af8b
leeedohyun <ldh19738@gmail.com>
2024-06-04 22:29:39 +0900

    test: 예약 추가 테스트

--- src/test/java/roomescape/reservation/presentation/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/presentation/ReservationControllerTest.java
@@ -1,5 +1,9 @@
 package roomescape.reservation.presentation;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ReservationControllerTest {
@@ -26,5 +33,28 @@ class ReservationControllerTest {
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("예약을 생성한다.")
+    void testCreateReservation() {
+        // given
+        Map<String, String> params = Map.of(
+                "name", "브라운",
+                "date", "2021-08-01",
+                "time", "15:40"
+        );
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(200);
+        assertThat(response.jsonPath().getLong("id")).isEqualTo(1L);
     }
 }

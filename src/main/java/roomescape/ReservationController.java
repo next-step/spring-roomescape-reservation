b0ec9a40c0fe60d697faafd9280010ad5a4cfcdf
yeeuniii <anna862700@gmail.com>
2024-06-06 01:46:41 +0900

    feat(step1): 예약 추가 api 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -3,16 +3,27 @@ package roomescape;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
     private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
 
     @GetMapping("reservations")
     public ResponseEntity<List<Reservation>> read() {
         return ResponseEntity.ok().body(reservations);
+    }
+
+    @PostMapping("reservations")
+    public ResponseEntity<Reservation> create(@RequestBody Reservation newReservation) {
+        Reservation reservation = Reservation.toEntity(newReservation, index.getAndIncrement());
+        reservations.add(reservation);
+        return ResponseEntity.ok().body(reservation);
     }
 }

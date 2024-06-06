79ae81c2186ec688abcf30e1e7643816e33ff8e9
eh <etkim02@naver.com>
2024-06-06 09:31:10 +0900

    refactor: 예약 관리 1-3. schema reservation_time 추가 및 reservation 테이블 변경
    - schema: reservation time_id 외래키 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,20 @@
+DROP TABLE IF EXISTS reservation_time;
+
+CREATE TABLE reservation_time
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 DROP TABLE IF EXISTS reservation;
 
 CREATE TABLE reservation
 (
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-    date    VARCHAR(255) NOT NULL,
-    time    VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date VARCHAR(255) NOT NULL,
+    time_id BIGINT,                           -- 컬럼 수정
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id) -- 외래키 추가
 );

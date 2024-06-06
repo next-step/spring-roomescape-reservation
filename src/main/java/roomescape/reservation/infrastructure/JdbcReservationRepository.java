11ec1ad74c1ae4b34e8c463839e9ddb588f7538b
leeedohyun <ldh19738@gmail.com>
2024-06-06 11:04:10 +0900

    refactor: 저장 로직에 SimpleJdbcInsert 적용

--- src/main/java/roomescape/reservation/infrastructure/JdbcReservationRepository.java
+++ src/main/java/roomescape/reservation/infrastructure/JdbcReservationRepository.java
@@ -1,13 +1,14 @@
 package roomescape.reservation.infrastructure;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.domain.repository.ReservationRepository;
 import roomescape.time.domain.ReservationTime;
@@ -16,32 +17,29 @@ import roomescape.time.domain.ReservationTime;
 public class JdbcReservationRepository implements ReservationRepository {
 
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
-    public JdbcReservationRepository(JdbcTemplate jdbcTemplate) {
+    public JdbcReservationRepository(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("reservation")
+                .usingGeneratedKeyColumns("id");
     }
 
     @Override
     public Reservation save(Reservation reservation) {
-        String sql = "INSERT INTO reservation(name, date, time_id) VALUES (?, ?, ?)";
-
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(con -> {
-            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, reservation.getName());
-            ps.setString(2, reservation.getDate().toString());
-            ps.setLong(3, reservation.getTime().getId());
-            return ps;
-        }, keyHolder);
-
-        Long id = keyHolder.getKey().longValue();
-        reservation.setId(id);
-        return reservation;
+        SqlParameterSource params = new BeanPropertySqlParameterSource(reservation);
+        Long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
+        System.out.println("id = " + id);
+        return new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getTime());
     }
 
     @Override
     public List<Reservation> findAll() {
-        String sql = "SELECT r.id, r.name, r.date, t.id as time_id, t.start_at FROM reservation as r INNER JOIN reservation_time as t ON r.time_id = t.id";
+        String sql = "SELECT r.id, r.name, r.date, t.id as time_id, t.start_at"
+                + " FROM reservation as r"
+                + " INNER JOIN reservation_time as t"
+                + " ON r.time_id = t.id";
         return jdbcTemplate.query(sql, (resultSet, rowNum) -> new Reservation(
                 resultSet.getLong("id"),
                 resultSet.getString("name"),
--- src/main/java/roomescape/time/infrastructure/JdbcReservationTimeRepository.java
+++ src/main/java/roomescape/time/infrastructure/JdbcReservationTimeRepository.java
@@ -1,41 +1,37 @@
 package roomescape.time.infrastructure;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import roomescape.time.domain.ReservationTime;
 
 @Repository
 public class JdbcReservationTimeRepository {
 
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
-    public JdbcReservationTimeRepository(JdbcTemplate jdbcTemplate) {
+    public JdbcReservationTimeRepository(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("reservation_time")
+                .usingGeneratedKeyColumns("id");
     }
 
     public ReservationTime save(ReservationTime reservationTime) {
-        String sql = "INSERT INTO reservation_time(start_at) VALUES (?)";
-
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(con -> {
-            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, reservationTime.getStartAt().toString());
-            return ps;
-        }, keyHolder);
-
-        Long id = keyHolder.getKey().longValue();
-        reservationTime.setId(id);
-        return reservationTime;
+        SqlParameterSource params = new BeanPropertySqlParameterSource(reservationTime);
+        Long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
+        return new ReservationTime(id, reservationTime.getStartAt());
     }
 
     public List<ReservationTime> findAll() {
-        String sql = "SELECT id, start_at FROM reservation_time";
+        String sql = "SELECT * FROM reservation_time";
         return jdbcTemplate.query(sql, (resultSet, rowNum) -> new ReservationTime(
                 resultSet.getLong("id"),
                 resultSet.getString("start_at")

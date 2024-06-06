ae5e97361a6eb5a29cd29449ee5d9167556f9f87
haero77 <shineecard5@gmail.com>
2024-06-07 00:36:09 +0900

    feat: 루트 경로, 예약 페이지 렌더링 기능 추가

---
+++ src/main/java/roomescape/admin/controller/AdminController.java
@@ -0,0 +1,19 @@
+package roomescape.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/")
+    public String home() {
+        return "admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String reservation() {
+        return "admin/reservation-legacy";
+    }
+
+}

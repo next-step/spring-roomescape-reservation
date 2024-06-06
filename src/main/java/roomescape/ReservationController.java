3f7e82447c479ebfba4de0027d7b2ef4e0d97142
yeeuniii <anna862700@gmail.com>
2024-06-07 02:19:18 +0900

    refactor(step1): 예약 조회 api jdbcTemplate 적용

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,6 +1,8 @@
 package roomescape;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,11 +13,29 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<Reservation> reservationRowMapper = (resultSet, rowNum) -> {
+        return new Reservation(
+                resultSet.getLong("id"),
+                resultSet.getNString("name"),
+                resultSet.getDate("date").toLocalDate(),
+                resultSet.getTime("time").toLocalTime()
+        );
+    };
+
     private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(1);
 
     @GetMapping("reservations")
     public ResponseEntity<List<Reservation>> read() {
+        String sql = "select * from reservation";
+        List<Reservation> reservations = jdbcTemplate.query(sql, reservationRowMapper);
+
         return ResponseEntity.ok().body(reservations);
     }
 

66a1b41746127ea5f0c5947e8e553c643fc7366e
JohnPrk <uo3641493@gmail.com>
2024-06-07 15:46:18 +0900

    chore : 예약 테이블 및 시간 테이블 연관관계 설정 세팅

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,16 @@
-create TABLE IF NOT EXISTS reservation
+create TABLE reservation_time
 (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    date VARCHAR(255) NOT NULL,
-    time VARCHAR(255) NOT NULL,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-create TABLE reservation_time
+create TABLE reservation
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date VARCHAR(255) NOT NULL,
+    time_id BIGINT,                           -- 컬럼 수정
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id) -- 외래키 추가
 );

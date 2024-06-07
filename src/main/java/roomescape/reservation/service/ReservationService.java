a6a1147e5aaaf70231b8eff3f845667a70dab1ed
JohnPrk <uo3641493@gmail.com>
2024-06-07 13:08:41 +0900

    feat : @Transactional 추가

--- src/main/java/roomescape/reservation/service/ReservationService.java
+++ src/main/java/roomescape/reservation/service/ReservationService.java
@@ -1,6 +1,7 @@
 package roomescape.reservation.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.domain.ReservationRepository;
 
@@ -15,19 +16,23 @@ public class ReservationService {
         this.reservationRepository = reservationRepository;
     }
 
+    @Transactional
     public Reservation save(Reservation reservation) {
         long id = reservationRepository.save(reservation);
         return findById(id);
     }
 
-    private Reservation findById(long id) {
+    @Transactional(readOnly = true)
+    public Reservation findById(long id) {
         return reservationRepository.findById(id);
     }
 
+    @Transactional(readOnly = true)
     public List<Reservation> findAll() {
         return reservationRepository.findAll();
     }
 
+    @Transactional
     public void delete(Long id) {
         reservationRepository.delete(id);
     }

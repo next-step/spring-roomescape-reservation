d852ad8ad0ed3b795a9f6ba8180be9acd93fb7a9
JohnPrk <uo3641493@gmail.com>
2024-06-07 10:56:31 +0900

    chore : 시간 테이블 스키마 세팅

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,15 @@
-CREATE TABLE IF NOT EXISTS reservation
+create TABLE IF NOT EXISTS reservation
 (
     id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     date VARCHAR(255) NOT NULL,
     time VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create TABLE reservation_time
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );

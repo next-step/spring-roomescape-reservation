be647a46ac8a34785af4739c08c60d3d9ba51e8b
dharana <dharana7723@gmail.com>
2024-06-08 01:36:26 +0900

    fix: theme 경로 수정 및 thymeleaf 설정 수정

---
+++ src/main/java/roomescape/admin/AdminController.java
@@ -0,0 +1,31 @@
+package roomescape.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/time")
+    public String time() {
+        return "admin/time";
+    }
+
+    @GetMapping("/theme")
+    public String theme() {
+        return "admin/theme";
+    }
+
+    @GetMapping("/reservation")
+    public String reservation(){
+        return "admin/reservation";
+    }
+
+}

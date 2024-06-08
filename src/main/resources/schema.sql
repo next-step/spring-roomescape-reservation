b1a80d08760fdeaf6d22079d514b1c54edcc91fa
dharana <dharana7723@gmail.com>
2024-06-09 05:43:29 +0900

    feat: 테마 테이블 생성 쿼리 추가 및 예약 테이블 생성시 테마 외래키 추카

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,13 @@
+CREATE TABLE theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
 CREATE TABLE reservation_time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
@@ -12,6 +22,8 @@ CREATE TABLE reservation
     name    VARCHAR(255) NOT NULL,
     date    VARCHAR(255) NOT NULL,
     time_id    BIGINT,
+    theme_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );

1f1fe47ab35381cf5e3e591b7f2fc4d9a7139577
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2024-06-08 12:03:10 +0900

    refactor: change reservation and add Theme

--- src/main/java/roomescape/apply/reservation/domain/Reservation.java
+++ src/main/java/roomescape/apply/reservation/domain/Reservation.java
@@ -1,6 +1,7 @@
 package roomescape.apply.reservation.domain;
 
 import roomescape.apply.reservationtime.domain.ReservationTime;
+import roomescape.apply.theme.domain.Theme;
 
 public class Reservation {
 
@@ -8,23 +9,26 @@ public class Reservation {
     private String name;
     private String date;
     private ReservationTime time;
+    private Theme theme;
 
     protected Reservation() {
 
     }
 
-    public Reservation(Long id, String name, String date, ReservationTime time) {
+    public Reservation(Long id, String name, String date, ReservationTime time, Theme theme) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+        this.theme = theme;
     }
 
-    public static Reservation of(String name, String date, ReservationTime time) {
+    public static Reservation of(String name, String date, ReservationTime time, Theme theme) {
         Reservation reservation = new Reservation();
         reservation.name = name;
         reservation.date = date;
         reservation.time = time;
+        reservation.theme = theme;
         return reservation;
     }
 
@@ -46,5 +50,9 @@ public class Reservation {
 
     public ReservationTime getTime() {
         return time;
+    }
+
+    public Theme getTheme() {
+        return theme;
     }
 }

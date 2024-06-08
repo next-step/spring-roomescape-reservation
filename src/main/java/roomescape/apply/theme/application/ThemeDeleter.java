913ca58464b3b17858dfa69f179886bbd8abe958
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2024-06-08 12:06:56 +0900

    feat: Implement Theme Application Layer
    - Add ThemeDeleter to delete theme.
    - Add ThemeFinder to find themes.
    - Add ThemeSaver to save new theme.

---
+++ src/main/java/roomescape/apply/theme/application/ThemeDeleter.java
@@ -0,0 +1,20 @@
+package roomescape.apply.theme.application;
+
+import org.springframework.stereotype.Service;
+import roomescape.apply.theme.domain.repository.ThemeRepository;
+import roomescape.apply.theme.application.exception.NotFoundThemeException;
+
+@Service
+public class ThemeDeleter {
+
+    private final ThemeRepository themeRepository;
+
+    public ThemeDeleter(ThemeRepository themeRepository) {
+        this.themeRepository = themeRepository;
+    }
+
+    public void deleteReservationTimeBy(long id) {
+        final long existId = themeRepository.checkIdExists(id).orElseThrow(NotFoundThemeException::new);
+        themeRepository.deleteById(existId);
+    }
+}

218365638a5502fc2c43ce38751110db93cf1976
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2024-06-08 12:08:03 +0900

    feat: Implement Theme Presentation Layer

---
+++ src/main/java/roomescape/apply/theme/ui/ThemeController.java
@@ -0,0 +1,37 @@
+package roomescape.apply.theme.ui;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import roomescape.apply.theme.application.ThemeDeleter;
+import roomescape.apply.theme.application.ThemeFinder;
+import roomescape.apply.theme.application.ThemeSaver;
+import roomescape.apply.theme.ui.dto.ThemeRequest;
+import roomescape.apply.theme.ui.dto.ThemeResponse;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/themes")
+public record ThemeController(ThemeSaver themeSaver,
+                              ThemeFinder themeFinder,
+                              ThemeDeleter themeDeleter) {
+
+    @GetMapping
+    public ResponseEntity<List<ThemeResponse>> findAllThemes() {
+        List<ThemeResponse> responses = themeFinder.findAll();
+        return ResponseEntity.ok(responses);
+    }
+
+    @PostMapping
+    public ResponseEntity<ThemeResponse> createTheme(@RequestBody ThemeRequest request) {
+        ThemeResponse themeResponse = themeSaver.saveThemeBy(request);
+        return ResponseEntity.ok(themeResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpEntity<Void> deleteTime(@PathVariable("id") long id) {
+        themeDeleter.deleteReservationTimeBy(id);
+        return ResponseEntity.ok().build();
+    }
+}

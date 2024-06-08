2685007ff48744267f90775807b89d6b0ec609e3
leeedohyun <ldh19738@gmail.com>
2024-06-08 20:04:24 +0900

    test: 테마 API 테스트

---
+++ src/test/java/roomescape/theme/presentation/ThemeControllerTest.java
@@ -0,0 +1,37 @@
+package roomescape.theme.presentation;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class ThemeControllerTest {
+
+    @Test
+    @DisplayName("테마를 생성한다.")
+    void testCreateTheme() {
+        Map<String, String> params = Map.of(
+                "name", "레벨2 탈출",
+                "description", "우테코 레벨2를 탈출하는 내용입니다.",
+                "thumbnail", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"
+        );
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/themes")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("id", notNullValue());
+    }
+}

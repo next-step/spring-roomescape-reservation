0ab9f632b6f494e0102d582c9c415f3b4eb57b2b
leeedohyun <ldh19738@gmail.com>
2024-06-08 19:55:52 +0900

    feat: Theme DDL 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,3 +14,12 @@ CREATE TABLE reservation
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id) -- 외래키 추가
 );
+
+CREATE TABLE theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);

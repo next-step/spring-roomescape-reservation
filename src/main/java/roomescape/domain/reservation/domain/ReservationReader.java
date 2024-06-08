950d4e523bbfb38288884d43e5360f7c1f7872b9
haero77 <shineecard5@gmail.com>
2024-06-08 21:45:40 +0900

    feat(ReservationQueryApi): 전체 예약 조회 API 구현

--- src/main/java/roomescape/domain/reservation/domain/ReservationReader.java
+++ src/main/java/roomescape/domain/reservation/domain/ReservationReader.java
@@ -7,6 +7,7 @@ import roomescape.domain.reservation.dto.ReservationId;
 import roomescape.domain.reservation.exception.ReservationNotFoundException;
 import roomescape.domain.reservation.repository.ReservationRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -19,6 +20,10 @@ public class ReservationReader {
         return findById(id).orElseThrow(
                 () -> new ReservationNotFoundException("Cannot find Reservation matching id=%d".formatted(id.value()))
         );
+    }
+
+    public List<Reservation> findAll() {
+        return repository.findAll();
     }
 
     private Optional<Reservation> findById(final ReservationId id) {
---
+++ src/main/java/roomescape/domain/reservation/service/ReservationQueryService.java
@@ -0,0 +1,22 @@
+package roomescape.domain.reservation.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import roomescape.domain.reservation.domain.ReservationReader;
+import roomescape.domain.reservation.domain.model.Reservation;
+import roomescape.domain.reservation.service.response.ReservationQueryResponse;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationQueryService {
+
+    private final ReservationReader reader;
+
+    public List<ReservationQueryResponse> findAll() {
+        final List<Reservation> reservations = reader.findAll();
+        return ReservationQueryResponse.from(reservations);
+    }
+
+}

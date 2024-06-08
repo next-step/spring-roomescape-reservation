49b74ad83c827c8652f45231677159069307dcd3
leeedohyun <ldh19738@gmail.com>
2024-06-08 20:03:25 +0900

    test: ThemeRepository 테스트

---
+++ src/test/java/roomescape/theme/infrastructure/JdbcThemeRepositoryTest.java
@@ -0,0 +1,43 @@
+package roomescape.theme.infrastructure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+
+import javax.sql.DataSource;
+import roomescape.theme.domain.Theme;
+
+@JdbcTest
+class JdbcThemeRepositoryTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    private JdbcThemeRepository jdbcThemeRepository;
+
+    @BeforeEach
+    void setUp() {
+        jdbcThemeRepository = new JdbcThemeRepository(dataSource);
+    }
+
+    @Test
+    @DisplayName("테마를 저장한다.")
+    void save() {
+        // given
+        Theme theme = new Theme("레벨2 탈출","우테코 레벨2를 탈출하는 내용입니다.",
+                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
+
+        // when
+        Theme savedTheme = jdbcThemeRepository.save(theme);
+
+        // then
+        assertThat(savedTheme.getId()).isNotNull();
+        assertThat(savedTheme).usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(theme);
+    }
+}

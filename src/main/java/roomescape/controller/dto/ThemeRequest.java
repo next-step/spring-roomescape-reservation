45116074a1f0b9b70a6c3f910642b3d5100ca573
eottabom <snoopy12oyk@gmail.com>
2024-06-10 14:19:54 +0900

    STEP2: 예외처리 및 리팩터링
    - fix: 에러코드 개선
    - fix: 테마 예외처리 추가

--- src/main/java/roomescape/controller/dto/ThemeRequest.java
+++ src/main/java/roomescape/controller/dto/ThemeRequest.java
@@ -1,5 +1,24 @@
 package roomescape.controller.dto;
 
+import roomescape.exception.ErrorCode;
+import roomescape.exception.RoomEscapeException;
+
+import org.springframework.util.ObjectUtils;
+
 public record ThemeRequest(String name, String description, String thumbnail) {
+
+	public static void validateThemeRequest(ThemeRequest request) {
+		if (ObjectUtils.isEmpty(request.name())) {
+			throw new RoomEscapeException(ErrorCode.INVALID_THEME_NAME);
+		}
+
+		if (ObjectUtils.isEmpty(request.description())) {
+			throw new RoomEscapeException(ErrorCode.INVALID_THEME_DESCRIPTION);
+		}
+
+		if (ObjectUtils.isEmpty(request.thumbnail())) {
+			throw new RoomEscapeException(ErrorCode.INVALID_THEME_THUMBNAIL);
+		}
+	}
 
 }

0d53bfb960918edc23f08035f4d75f3aef51c0f7
younghyun <younghyunjo99@gmail.com>
2024-06-11 22:13:59 +0900

    feat(AdminController):AdminController 추가

---
+++ src/main/java/roomescape/admin/AdminController.java
@@ -0,0 +1,15 @@
+package roomescape.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("reservation")
+    public String reservation(Model model) {
+        return "admin/reservation-legacy";
+    }
+}

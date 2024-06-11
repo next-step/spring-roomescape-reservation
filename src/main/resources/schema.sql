7cc2618456373e8ba01e0c0ffaae2e68c56aff31
jsween5723 <jsween5723@gmail.com>
2024-06-11 11:26:56 +0900

    feat(reservation): reservation time 외래키 관계 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,8 +3,9 @@ CREATE TABLE reservation
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    VARCHAR(255) NOT NULL,
-    time    VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    time_id BIGINT,                           -- 컬럼 수정
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id) -- 외래키 추가
 );
 
 CREATE TABLE reservation_time

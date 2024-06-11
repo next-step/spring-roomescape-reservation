1bad707041034a9ca55d091f79b444c56caf112e
kilian <audrb96@gmail.com>
2024-06-11 20:17:38 +0900

    refactor : 테스트 패키지 이동

---
+++ src/test/java/roomescape/application/api/ReservationTimeCommandApiTest.java
@@ -0,0 +1,5 @@
+package roomescape.application.api;
+
+class ReservationTimeCommandApiTest {
+
+}
--- src/test/java/roomescape/application/controller/AdminPageControllerTest.java
+++ src/test/java/roomescape/application/controller/AdminPageControllerTest.java
@@ -1,4 +1,4 @@
-package roomescape.controller;
+package roomescape.application.controller;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;

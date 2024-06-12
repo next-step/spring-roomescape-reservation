bbdce905066dcd5a917f52b90f843210242330f5
kilian <audrb96@gmail.com>
2024-06-12 18:22:29 +0900

    feature : 스키마 reservation_time 추가

--- src/main/resources/db/schema/schema.sql
+++ src/main/resources/db/schema/schema.sql
@@ -1,9 +1,20 @@
 DROP TABLE IF EXISTS reservation;
+DROP TABLE IF EXISTS reservation_time;
+
 
 CREATE TABLE reservation
 (
     id        BIGINT       NOT NULL AUTO_INCREMENT,
     name      VARCHAR(255) NOT NULL,
     date_time DATETIME     NOT NULL,
+    time_id   BIGINT,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE reservation_time
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );

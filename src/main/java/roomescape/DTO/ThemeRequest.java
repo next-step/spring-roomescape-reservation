e2ad8260ea5cd4e4f5f3cdbd217859674a96fac8
yeeuniii <anna862700@gmail.com>
2024-06-12 15:09:07 +0900

    feat(step2): theme 응답/요청 DTO, entity 생성

---
+++ src/main/java/roomescape/DTO/ThemeRequest.java
@@ -0,0 +1,25 @@
+package roomescape.DTO;
+
+public class ThemeRequest {
+    private final String name;
+    private final String description;
+    private final String thumbnail;
+
+    public ThemeRequest(String name, String description, String thumbnail) {
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+}
---
+++ src/main/java/roomescape/DTO/ThemeResponse.java
@@ -0,0 +1,46 @@
+package roomescape.DTO;
+
+import roomescape.Entity.Theme;
+
+import java.util.List;
+
+public class ThemeResponse {
+    private final Long id;
+    private final String name;
+    private final String description;
+    private final String thumbnail;
+
+    public ThemeResponse(Long id, String name, String description, String thumbnail) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    public ThemeResponse(Theme theme) {
+        this.id = theme.getId();
+        this.name = theme.getName();
+        this.description = theme.getDescription();
+        this.thumbnail = theme.getThumbnail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public List<ThemeResponse> toDTOList(List<Theme> themes) {
+        return themes.stream().map(ThemeResponse::new).toList();
+    }
+}

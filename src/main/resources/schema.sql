7e8ff149b56c6d370ad085c09f2160e99700dc00
yeeuniii <anna862700@gmail.com>
2024-06-12 14:55:40 +0900

    feat(step2): theme 스키마 추가 및 reservation 스키마 재정의

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,12 +5,23 @@ CREATE TABLE reservation_time
     PRIMARY KEY (id)
 );
 
+CREATE TABLE theme
+(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE reservation
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     date VARCHAR(255) NOT NULL,
     time_id BIGINT,
+    theme_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id) ON DELETE CASCADE
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id) ON DELETE CASCADE,
+    FOREIGN KEY (theme_id) REFERENCES theme (id) ON DELETE CASCADE
 );

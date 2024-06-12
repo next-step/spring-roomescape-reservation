ecf0fc054e85b568480714ca21c6efce985111ab
psy3720 <qkrtmddyd12@naver.com>
2024-06-12 23:27:55 +0900

    refactor: 시간예외 검증 서비스클래스로 이동

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,11 +1,5 @@
 package roomescape.domain;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import org.springframework.http.HttpStatus;
-import roomescape.exception.BusinessException;
-
 public class Reservation {
     private Long id;
     private String name;
@@ -25,25 +19,12 @@ public class Reservation {
     }
 
     public Reservation(String name, String date, ReservationTime time, ReservationTheme theme) {
-        if (isDateExpired(date, time.getStartAt())) {
-            throw new BusinessException("이미 지나간 날짜는 예약할 수 없습니다.", HttpStatus.UNPROCESSABLE_ENTITY);
-        }
-
         this.name = name;
         this.date = date;
         this.time = time;
         this.theme = theme;
     }
 
-    private boolean isDateExpired(String date, String startAt) {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime reservationDateTime = LocalDateTime.of(LocalDate.parse(date), LocalTime.parse(startAt));
-
-        if (reservationDateTime.isBefore(now)) {
-            return true;
-        }
-        return false;
-    }
 
     public Long getId() {
         return id;
--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,7 +1,9 @@
 package roomescape.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -29,32 +31,19 @@ public class ReservationService {
         this.reservationThemeDao = reservationThemeDao;
     }
 
-    public ReservationResponse createReservation(ReservationRequest request) {
-        Optional<ReservationTime> findReservationTime = reservationTimeDao
-                .findById(Long.parseLong(request.getTimeId()));
-        if (findReservationTime.isEmpty()) {
-            throw new BusinessException("존재하지 않는 예약시간입니다.", HttpStatus.BAD_REQUEST);
-        }
+    public ReservationResponse createReservation(ReservationRequest reservationRequest) {
+        ReservationTime reservationTime = findReservationTimeById(reservationRequest.getTimeId());
+        findReservationThemeById(reservationRequest.getThemeId());
 
-        Optional<ReservationTheme> findReservationTheme = reservationThemeDao
-                .findById(Long.parseLong(request.getThemeId()));
-        if (findReservationTheme.isEmpty()) {
-            throw new BusinessException("존재하지 않는 예약테마입니다.", HttpStatus.BAD_REQUEST);
-        }
+        validateReservationCreation(reservationRequest, reservationTime);
 
-        long count = reservationDao.countByDateAndTimeId(request.getDate(), request.getTimeId());
-        if (count > 0) {
-            throw new BusinessException("해당 시간에 이미 예약이 존재합니다.", HttpStatus.CONFLICT);
-        }
-
-        Reservation reservation = reservationDao.save(this.convertToEntity(request));
+        Reservation reservation = reservationDao.save(this.convertToEntity(reservationRequest));
         return this.convertToResponse(reservation);
     }
 
     public List<ReservationResponse> findAllReservations() {
-        return reservationDao.findAll()
-                .stream()
-                .map(reservation -> this.convertToResponse(reservation))
+        return reservationDao.findAll().stream()
+                .map(this::convertToResponse)
                 .collect(Collectors.toUnmodifiableList());
     }
 
@@ -62,25 +51,47 @@ public class ReservationService {
         reservationDao.delete(id);
     }
 
+    private void validateReservationCreation(ReservationRequest reservationRequest, ReservationTime reservationTime) {
+        long count = reservationDao.countByDateAndTimeId(reservationRequest.getDate()
+                , reservationRequest.getTimeId());
+        if (count > 0) {
+            throw new BusinessException("해당 시간에 이미 예약이 존재합니다.", HttpStatus.CONFLICT);
+        }
+
+        if (isDateExpired(reservationRequest.getDate(), reservationTime.getStartAt())) {
+            throw new BusinessException("이미 지나간 날짜는 예약할 수 없습니다.", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+
     private ReservationResponse convertToResponse(Reservation reservation) {
-        return new ReservationResponse(
-                reservation.getId()
-                , reservation.getName()
-                , reservation.getDate()
-                , reservation.getTime().getStartAt()
-                , reservation.getTheme().getName());
+        return new ReservationResponse(reservation.getId(), reservation.getName(), reservation.getDate(),
+                reservation.getTime().getStartAt(), reservation.getTheme().getName());
     }
 
     private Reservation convertToEntity(ReservationRequest reservationRequest) {
-        ReservationTime reservationTime = reservationTimeDao
-                .findById(Long.parseLong(reservationRequest.getTimeId())).get();
-        ReservationTheme reservationTheme = reservationThemeDao.findById(
-                Long.parseLong(reservationRequest.getThemeId())).get();
+        ReservationTime reservationTime = findReservationTimeById(reservationRequest.getTimeId());
+        ReservationTheme reservationTheme = findReservationThemeById(reservationRequest.getThemeId());
 
-        return new Reservation(
-                reservationRequest.getName()
-                , reservationRequest.getDate()
-                , reservationTime
-                , reservationTheme);
+        return new Reservation(reservationRequest.getName(), reservationRequest.getDate(), reservationTime,
+                reservationTheme);
+    }
+
+    private ReservationTime findReservationTimeById(String timeId) {
+        return reservationTimeDao
+                .findById(Long.parseLong(timeId))
+                .orElseThrow(() -> new BusinessException("존재하지 않는 예약시간입니다.", HttpStatus.BAD_REQUEST));
+    }
+
+    private ReservationTheme findReservationThemeById(String themeId) {
+        return reservationThemeDao
+                .findById(Long.parseLong(themeId))
+                .orElseThrow(() -> new BusinessException("존재하지 않는 예약테마입니다.", HttpStatus.BAD_REQUEST));
+    }
+
+    private boolean isDateExpired(String date, String startAt) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime reservationDateTime = LocalDateTime.of(LocalDate.parse(date), LocalTime.parse(startAt));
+
+        return reservationDateTime.isBefore(now);
     }
 }

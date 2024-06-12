d24ac0a6ccfd7b060359e6d3591515b5fd54a7f0
younghyun <younghyunjo99@gmail.com>
2024-06-13 06:27:34 +0900

    feat:쿼리 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,12 +5,12 @@ CREATE TABLE reservation_time
     PRIMARY KEY (id)
 );
 
-
 CREATE TABLE reservation
 (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    VARCHAR(255) NOT NULL,
     time_id BIGINT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id) -- 외래키 추가
 );

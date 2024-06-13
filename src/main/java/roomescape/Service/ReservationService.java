165bdbf6af4b9fd3a4dafad4425d4578cdfbfabb
yeeuniii <anna862700@gmail.com>
2024-06-14 01:49:49 +0900

    feat(step2): 예약 생성 시 요청 유효성 검사

--- src/main/java/roomescape/Service/ReservationService.java
+++ src/main/java/roomescape/Service/ReservationService.java
@@ -1,22 +1,36 @@
 package roomescape.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import roomescape.DTO.ReservationRequest;
 import roomescape.DTO.ReservationResponse;
 import roomescape.Entity.Reservation;
+import roomescape.Exception.BadRequestException;
 import roomescape.Exception.NotFoundException;
 import roomescape.Repository.ReservationRepository;
+import roomescape.Repository.ReservationTimeRepository;
+import roomescape.Repository.ThemeRepository;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @Service
 public class ReservationService {
     private final ReservationRepository reservationRepository;
+    private final ReservationTimeRepository reservationTimeRepository;
+    private final ThemeRepository themeRepository;
 
     @Autowired
-    public ReservationService(ReservationRepository reservationRepository) {
+    public ReservationService(
+            ReservationRepository reservationRepository,
+            ReservationTimeRepository reservationTimeRepository,
+            ThemeRepository themeRepository
+    ) {
         this.reservationRepository = reservationRepository;
+        this.reservationTimeRepository = reservationTimeRepository;
+        this.themeRepository = themeRepository;
     }
 
     public List<ReservationResponse> findAll() {
@@ -34,12 +48,55 @@ public class ReservationService {
     }
 
     public long make(ReservationRequest request) {
+        validateRequest(request);
         return reservationRepository.save(
                 request.getName(),
                 request.getDate(),
                 request.getTimeId(),
                 request.getThemeId()
         );
+    }
+
+    private void validateRequest(ReservationRequest request) {
+        validateDate(request.getDate());
+        checkExistentTime(request.getTimeId());
+        checkExistentTheme(request.getThemeId());
+    }
+
+    private void validateDate(String date) {
+        LocalDate reservationDate;
+        LocalDate now = LocalDate.now();
+
+        try {
+            reservationDate = LocalDate.parse(date);
+        }
+        catch (DateTimeParseException exception) {
+            throw new BadRequestException("유효하지 않은 날짜 형식입니다.");
+        }
+        if (reservationDate.isBefore(now)) {
+            throw new BadRequestException("이미 지난 날짜입니다.");
+        }
+        if (reservationDate.isEqual(now)) {
+            throw new BadRequestException("당일 예약은 불가능합니다.");
+        }
+    }
+
+    private void checkExistentTime(Long timeId) {
+        try {
+            reservationTimeRepository.findById(timeId);
+        }
+        catch (EmptyResultDataAccessException exception) {
+            throw new NotFoundException("존재하지 않는 예약 시간입니다.");
+        }
+    }
+
+    private void checkExistentTheme(Long themeId) {
+        try {
+            themeRepository.findById(themeId);
+        }
+        catch (EmptyResultDataAccessException exception) {
+            throw new NotFoundException("존재하지 않는 테마입니다.");
+        }
     }
 
     public void cancel(Long id) {

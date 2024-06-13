f444a07e7e9c62a14e9d8e40be275923bca95656
yeeuniii <anna862700@gmail.com>
2024-06-13 22:08:30 +0900

    feat(step2): BadRequest 상태 예외 핸들러 추가

--- src/main/java/roomescape/GlobalExceptionHandler.java
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package roomescape;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import roomescape.DTO.ExceptionResponse;
+import roomescape.Exception.BadRequestException;
 import roomescape.Exception.NotFoundException;
 
 @ControllerAdvice
@@ -13,6 +15,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException exception) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
+                .body(new ExceptionResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler({EmptyResultDataAccessException.class, BadRequestException.class})
+    public ResponseEntity<ExceptionResponse> handleBadRequestException(Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(new ExceptionResponse(exception.getMessage()));
     }
 }

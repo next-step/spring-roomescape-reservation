10fe4d634570f87c9e038d0a549e39386115b8b3
kilian <audrb96@gmail.com>
2024-06-13 16:36:31 +0900

    test : RestAssured 오류 수정

--- src/test/java/roomescape/application/controller/AdminPageControllerTest.java
+++ src/test/java/roomescape/application/controller/AdminPageControllerTest.java
@@ -3,24 +3,14 @@ package roomescape.application.controller;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AdminPageControllerTest {
-
-    @LocalServerPort
-    private int port;
-
-    @BeforeEach
-    void beforeEach() {
-        RestAssured.port = port;
-    }
 
     @Test
     void indexPage() {

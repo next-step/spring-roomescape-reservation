2c917fdc5912890b88ec25e7be6bf2caa2a34bd1
yeeuniii <anna862700@gmail.com>
2024-06-13 17:02:01 +0900

    test(step2): 예약 api 테스트코드 구현

---
+++ src/test/java/roomescape/ReservationTest.java
@@ -0,0 +1,75 @@
+package roomescape;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.DTO.ReservationRequest;
+import roomescape.DTO.ReservationResponse;
+import roomescape.DTO.ReservationTimeRequest;
+import roomescape.DTO.ReservationTimeResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationTest {
+    @Test
+    @DisplayName("ReservationController - create()")
+    void 예약() {
+        String name = "yeeun";
+        String date = "2024-12-13";
+        new ReservationTimeTest().예약_시간_생성();
+        new ThemeTest().테마_생성();
+
+        var response = RestAssured
+                .given().log().all()
+                .body(new ReservationRequest(name, date, 1L, 1L))
+                .contentType(ContentType.JSON)
+                .when().post("/reservations")
+                .then().log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    @DisplayName("ReservationController - read()")
+    void 젼체_예약_조회() {
+        예약();
+
+        var response = RestAssured
+                .given().log().all()
+                .when().get("/reservations")
+                .then().log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("", ReservationResponse.class)).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("ReservationController - delete() 1")
+    void 예약_취소() {
+        예약();
+
+        var response = RestAssured
+                .given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    @DisplayName("ReservationController - delete() 2")
+    void 존재하지_않는_예약_취소() {
+        var response = RestAssured
+                .given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+}

368b412672e93681d0bd85119b3b43723b736db6
yeeuniii <anna862700@gmail.com>
2024-06-13 22:41:58 +0900

    test(step2): theme 예외 처리에 대한 테스트 구현

--- src/test/java/roomescape/ThemeTest.java
+++ src/test/java/roomescape/ThemeTest.java
@@ -37,7 +37,25 @@ public class ThemeTest {
     }
 
     @Test
-    @DisplayName("ThemeController - read()")
+    @DisplayName("ThemeController - create() : duplicated name")
+    void 중복_이름_테마_생성() {
+        테마_생성();
+        String name = "수키도키";
+        String description = "흐르는 대로 살자 해파리처럼🪼";
+        String thumbnail = "https://pbs.twimg.com/media/GApx6fjagAAkFsX.jpg";
+
+        var response = RestAssured
+                .given().log().all()
+                .body(new ThemeRequest(name, description, thumbnail))
+                .contentType(ContentType.JSON)
+                .when().post("/themes")
+                .then().log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("ThemeController - read() : all")
     void 전체_테마_조회() {
         테마_생성();
 
@@ -51,7 +69,7 @@ public class ThemeTest {
     }
 
     @Test
-    @DisplayName("ThemeController - delete() 1")
+    @DisplayName("ThemeController - delete()")
     void 테마_삭제() {
         테마_생성();
 
@@ -64,7 +82,7 @@ public class ThemeTest {
     }
 
     @Test
-    @DisplayName("ThemeController - delete() 2")
+    @DisplayName("ThemeController - delete() : non existent theme")
     void 존재하지_않는_테마_삭제() {
         var response = RestAssured
                 .given().log().all()
@@ -72,5 +90,18 @@ public class ThemeTest {
                 .then().log().all().extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    @DisplayName("ThemeController - delete() : theme with reservation existing")
+    void 예약_존재하는_테마_삭제() {
+        new ReservationTest().예약();
+
+        var response = RestAssured
+                .given().log().all()
+                .when().delete("/themes/1")
+                .then().log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }

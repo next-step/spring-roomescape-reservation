a797eacf6b3106e5bbf7454c4679253ea0cd30a9
yeeuniii <anna862700@gmail.com>
2024-06-13 22:11:17 +0900

    feat(step2): theme 예외 처리 - 특정 테마에 대한 예약 있는 경우 테마 삭제, 이름 중복

--- src/main/java/roomescape/Service/ThemeService.java
+++ src/main/java/roomescape/Service/ThemeService.java
@@ -1,10 +1,12 @@
 package roomescape.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import roomescape.DTO.ThemeRequest;
 import roomescape.DTO.ThemeResponse;
 import roomescape.Entity.Theme;
+import roomescape.Exception.BadRequestException;
 import roomescape.Exception.NotFoundException;
 import roomescape.Repository.ThemeRepository;
 
@@ -21,6 +23,10 @@ public class ThemeService {
 
     public List<ThemeResponse> findAll() {
         List<Theme> themes = themeRepository.findAll();
+
+        if (themes.isEmpty()) {
+            throw new NotFoundException("등록된 테마가 없습니다.");
+        }
         return ThemeResponse.toDTOList(themes);
     }
 
@@ -30,14 +36,32 @@ public class ThemeService {
     }
 
     public long add(ThemeRequest request) {
+        validateDuplicated(request.getName());
         return themeRepository.save(request);
     }
 
+    private void validateDuplicated(String name) {
+        try {
+            Theme theme = themeRepository.findByName(name);
+
+            throw new BadRequestException("이미 존재하는 테마입니다.");
+        }
+        catch (EmptyResultDataAccessException ignored) {
+        }
+    }
+
     public void delete(Long id) {
+        checkMatchingReservation(id);
         long deleteCount = themeRepository.deleteById(id);
 
         if (deleteCount == 0) {
             throw new NotFoundException("id에 일치하는 테마가 없습니다.");
+        }
+    }
+
+    private void checkMatchingReservation(Long id) {
+        if (themeRepository.countReservationMatchWith(id) > 0) {
+            throw new BadRequestException("해당 테마에 대한 예약이 존재합니다.");
         }
     }
 }

a83a7a445a45d31fbca06194279ee19b637a27df
eh <etkim02@naver.com>
2024-06-13 22:24:21 +0900

    test: service ApplicationContext를 통한 Bean 가져온 후 테스트 진행

--- src/test/java/roomescape/admin/service/ReservationServiceTest.java
+++ src/test/java/roomescape/admin/service/ReservationServiceTest.java
@@ -1,136 +1,123 @@
 package roomescape.admin.service;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
+import org.springframework.context.ApplicationContext;
 import roomescape.admin.dto.*;
-import roomescape.admin.entity.ReservationTime;
-import roomescape.admin.entity.Theme;
-import roomescape.admin.repository.ReservationRepository;
+import roomescape.common.exception.PolicyException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static roomescape.common.exception.PolicyException.DUPLICATE_RESERVATION_ERROR;
+import static roomescape.common.exception.PolicyException.RESERVATION_PRIOR_TO_CURRENT_TIME_ERROR;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class ReservationServiceTest {
 
+    private final ThemeService themeService;
+    private final ReservationTimeService reservationTimeService;
+    private final ReservationService reservationService;
+
+    public ReservationServiceTest(ApplicationContext context){
+        this.themeService = context.getBean(ThemeService.class);
+        this.reservationTimeService = context.getBean(ReservationTimeService.class);
+        this.reservationService = context.getBean(ReservationService.class);
+    }
+
+    void given_테마_삭제(long id){
+        this.themeService.deleteTheme(id);
+    }
+
+    void given_예약시간_삭제(long id){
+        this.reservationTimeService.deleteReservationTime(id);
+    }
+
+    void given_예약_삭제(long id){
+        this.reservationService.deleteReservation(id);
+    }
+
     @Nested
     @DisplayName("방탈출 예약은")
     class Describe_Reservation{
-        long given_예약시간_id;
-
-        long given_지나간_예약시간_id;
-
-
+        long given_예약_한시간전_id;
         long given_테마_id;
+        long given_예약시간_10_00_id;
+        long given_예약_id;
 
+        void given_테마(){
+            given_테마_삭제(given_테마_id);
+            SaveThemeRequest given_테마 = new SaveThemeRequest("테마", "테마에 대한 설명", "good/wow.png");
+            ReadThemeResponse readThemeResponse = themeService.saveTheme(given_테마);
 
-        void given_예약_시간_한시간전(){
+            this.given_테마_id = readThemeResponse.id();
+        }
+
+        void given_예약시간_10_00(){
+            given_예약시간_삭제(this.given_예약시간_10_00_id);
+            SaveReservationTimeRequest given_예약시간_10_00 = new SaveReservationTimeRequest("10:00");
+            ReadReservationTimeResponse reservationTime = reservationTimeService.saveReservationTime(given_예약시간_10_00);
+
+            this.given_예약시간_10_00_id = reservationTime.id();
+        }
+
+        void given_예약_한시간전(){
+            given_예약_삭제(this.given_예약_한시간전_id);
+
             LocalDateTime pastTime = LocalDateTime.now().minusHours(1);
             String formattedTime = pastTime.format(DateTimeFormatter.ofPattern("hh:mm"));
 
-            SaveReservationTimeRequest given_예약시간_10_00 = new SaveReservationTimeRequest(formattedTime);
+            SaveReservationTimeRequest given_예약_한시간_전 = new SaveReservationTimeRequest(formattedTime);
+            ReadReservationTimeResponse reservationTime = reservationTimeService.saveReservationTime(given_예약_한시간_전);
 
-            var response = RestAssured.given().log().all()
-                    .body(given_예약시간_10_00)
-                    .contentType(ContentType.JSON)
-                    .when().post("/times");
-
-            given_지나간_예약시간_id = response.jsonPath().getLong("id");
+            this.given_예약_한시간전_id = reservationTime.id();
         }
 
-        void given_예약_시간_10_00(){
-            SaveReservationTimeRequest given_예약시간_10_00 = new SaveReservationTimeRequest("10:00");
-
-            var response = RestAssured.given().log().all()
-                    .body(given_예약시간_10_00)
-                    .contentType(ContentType.JSON)
-                    .when().post("/times");
-
-            given_예약시간_id = response.jsonPath().getLong("id");
-        }
-
-        void given_테마(){
-            SaveThemeRequest given_테마 = new SaveThemeRequest("테마", "테마에 대한 설명", "good/wow.png");
-
-            var response = RestAssured.given().log().all()
-                    .body(given_테마)
-                    .contentType(ContentType.JSON)
-                    .when().post("/themes");
-
-            given_테마_id = response.jsonPath().getLong("id");
-        }
-
-        void given_예약_4월_1일_10시(){
-            given_예약_시간_10_00();
+        void given_예약_2024_12_14(){
+            given_예약_삭제(this.given_예약_id);
             given_테마();
-            SaveReservationRequest given_예약_2024_04_01 = new SaveReservationRequest("김민기","2024-04-01", given_예약시간_id, given_테마_id);
+            given_예약시간_10_00();
+            SaveReservationRequest given_예약_2024_12_12 = new SaveReservationRequest("김민기","2024-12-14", given_예약시간_10_00_id, given_테마_id);
 
-            RestAssured.given().log().all()
-                    .body(given_예약_2024_04_01)
-                    .contentType(ContentType.JSON)
-                    .when().post("/reservations");
+            ReadReservationResponse reservation = reservationService.saveReservation(given_예약_2024_12_12);
+
+            this.given_예약_id = reservation.id();
         }
 
         @Test
         @DisplayName("중복된 예약을 할 경우, 에러를 리턴합니다.")
         void it_returns_error_for_duplicate_reservation(){
-            given_예약_4월_1일_10시();
+            given_예약_2024_12_14();
+            SaveReservationRequest given_예약_2024_12_14= new SaveReservationRequest("김민기","2024-12-14", given_예약시간_10_00_id, given_테마_id);
 
-            SaveReservationRequest given_예약_2024_04_01= new SaveReservationRequest("김민기","2024-04-01", given_예약시간_id, given_테마_id);
-
-            var response = RestAssured
-                    .given().log().all()
-                    .body(given_예약_2024_04_01)
-                    .contentType(ContentType.JSON)
-                    .when().post("/reservations")
-                    .then().log().all().extract();
-
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            assertThatThrownBy(() -> reservationService.saveReservation(given_예약_2024_12_14))
+                    .isInstanceOf(PolicyException.class)
+                    .hasMessage(DUPLICATE_RESERVATION_ERROR);
         }
 
         @Test
         @DisplayName("지나간 시간으로 예약을 하면, 에러를 리턴합니다.")
         void it_returns_error_for_past_reservation_time(){
-            given_예약_시간_한시간전();
-            given_테마();
-            SaveReservationRequest given_예약_2024_04_01= new SaveReservationRequest("김민기","2024-04-01", given_지나간_예약시간_id, given_테마_id);
+            given_예약_한시간전();
+            SaveReservationRequest given_예약_2024_04_01= new SaveReservationRequest("김민기","2024-04-01", given_예약_한시간전_id, given_테마_id);
 
-            var response = RestAssured
-                    .given().log().all()
-                    .body(given_예약_2024_04_01)
-                    .contentType(ContentType.JSON)
-                    .when().post("/reservations")
-                    .then().log().all().extract();
-
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            assertThatThrownBy(() -> reservationService.saveReservation(given_예약_2024_04_01))
+                    .isInstanceOf(PolicyException.class)
+                    .hasMessage(RESERVATION_PRIOR_TO_CURRENT_TIME_ERROR);
         }
 
         @Test
         @DisplayName("지나간 날짜로 예약을 하면, 에러를 리턴합니다.")
         void it_returns_error_for_past_reservation_date(){
-            given_예약_시간_한시간전();
-            given_테마();
-            SaveReservationRequest given_예약_2024_01_01= new SaveReservationRequest("김민기","2024-01-01", given_지나간_예약시간_id, given_테마_id);
+            given_예약시간_10_00();
+            SaveReservationRequest given_예약_2024_01_01= new SaveReservationRequest("김민기","2024-01-01", given_예약시간_10_00_id, given_테마_id);
 
-            var response = RestAssured
-                    .given().log().all()
-                    .body(given_예약_2024_01_01)
-                    .contentType(ContentType.JSON)
-                    .when().post("/reservations")
-                    .then().log().all().extract();
-
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            assertThatThrownBy(() -> reservationService.saveReservation(given_예약_2024_01_01))
+                    .isInstanceOf(PolicyException.class)
+                    .hasMessage(RESERVATION_PRIOR_TO_CURRENT_TIME_ERROR);
         }
     }
 }
--- src/test/java/roomescape/admin/service/ThemeServiceTest.java
+++ src/test/java/roomescape/admin/service/ThemeServiceTest.java
@@ -1,103 +1,110 @@
 package roomescape.admin.service;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import roomescape.admin.dto.SaveReservationRequest;
-import roomescape.admin.dto.SaveReservationTimeRequest;
-import roomescape.admin.dto.SaveThemeRequest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import roomescape.admin.dto.*;
+import roomescape.common.exception.PolicyException;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static roomescape.common.exception.PolicyException.THEME_IN_USE_RESERVATION_ERROR;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class ThemeServiceTest {
+
+    private final ThemeService themeService;
+    private final ReservationTimeService reservationTimeService;
+    private final ReservationService reservationService;
+
+    public ThemeServiceTest(ApplicationContext context){
+        this.themeService = context.getBean(ThemeService.class);
+        this.reservationTimeService = context.getBean(ReservationTimeService.class);
+        this.reservationService = context.getBean(ReservationService.class);
+    }
+
+    void given_테마_삭제(long id){
+        this.themeService.deleteTheme(id);
+    }
+
+    void given_예약시간_삭제(long id){
+        this.reservationTimeService.deleteReservationTime(id);
+    }
+
+    void given_예약_삭제(long id){
+        this.reservationService.deleteReservation(id);
+    }
+
     @Nested
     @DisplayName("테마는")
     class Describe_Theme{
         @Nested
         @DisplayName("예약된 목록에서 사용중이라면")
         class Context_with_in_use_reservations{
-            long given_삭제_예약시간_id;
-            long given_테마_id;
+            long given_삭제_테마_id;
+            long given_예약시간_id;
+            long given_예약_id;
 
-            void given_예약시간_10_20(){
-                SaveReservationTimeRequest 예약시간_10_20 = new SaveReservationTimeRequest("10:20");
+            void given_삭제_테마(){
+                given_테마_삭제(this.given_삭제_테마_id);
+                SaveThemeRequest given_테마 = new SaveThemeRequest("테마", "테마에 대한 설명", "good/wow.png");
+                ReadThemeResponse readThemeResponse = themeService.saveTheme(given_테마);
 
-                var response = RestAssured.given().log().all()
-                        .body(예약시간_10_20)
-                        .contentType(ContentType.JSON)
-                        .when().post("/times")
-                        .then().log().all().extract();
-
-                given_삭제_예약시간_id = response.jsonPath().getLong("id");
+                this.given_삭제_테마_id = readThemeResponse.id();
             }
 
-            void given_테마(){
-                SaveThemeRequest 테마 = new SaveThemeRequest("테마","테마설명","썸네일 URL");
+            void given_예약시간(){
+                given_예약시간_삭제(this.given_예약시간_id);
+                SaveReservationTimeRequest given_예약시간_10_00 = new SaveReservationTimeRequest("10:00");
+                ReadReservationTimeResponse reservationTime = reservationTimeService.saveReservationTime(given_예약시간_10_00);
 
-                var response = RestAssured.given().log().all()
-                        .body(테마)
-                        .contentType(ContentType.JSON)
-                        .when().post("/themes")
-                        .then().log().all().extract();
-
-                given_테마_id = response.jsonPath().getLong("id");
+                this.given_예약시간_id = reservationTime.id();
             }
 
-            void given_예약_2024_12_12(){
-                given_테마();
-                given_예약시간_10_20();
-                SaveReservationRequest given_예약_2024_12_12 = new SaveReservationRequest("김민기","2024-12-12", given_삭제_예약시간_id, given_테마_id);
+            void given_예약_2024_12_13_id(){
+                given_예약_삭제(this.given_예약_id);
+                given_삭제_테마();
+                given_예약시간();
+                SaveReservationRequest given_예약_2024_12_12 = new SaveReservationRequest("김민기","2024-12-13", given_예약시간_id, given_삭제_테마_id);
+                ReadReservationResponse reservation = reservationService.saveReservation(given_예약_2024_12_12);
 
-                RestAssured.given().log().all()
-                        .body(given_예약_2024_12_12)
-                        .contentType(ContentType.JSON)
-                        .when().post("/reservations")
-                        .then().log().all().extract();
+                this.given_예약_id = reservation.id();
             }
 
             @Test
             @DisplayName("삭제할 수 없고, 에러를 리턴합니다.")
             void it_return_error_and_not_delete_theme(){
-                given_예약_2024_12_12();
+                given_예약_2024_12_13_id();
 
-                var response = RestAssured
-                        .given().log().all()
-                        .contentType(ContentType.JSON)
-                        .when().delete("/themes/"+given_테마_id)
-                        .then().log().all().extract();
-
-                assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+                assertThatThrownBy(() ->  themeService.deleteTheme(given_삭제_테마_id))
+                        .isInstanceOf(PolicyException.class)
+                        .hasMessage(THEME_IN_USE_RESERVATION_ERROR);
             }
         }
 
         @Nested
         @DisplayName("예약된 목록에서 사용하지 않는다면")
         class Context_not_use_theme_you_are_deleting_for_reservation{
-            void given_테마(){
-                SaveThemeRequest 테마 = new SaveThemeRequest("테마","테마설명","썸네일 URL");
+            long given_테마_id;
 
-                var response = RestAssured.given().log().all()
-                        .body(테마)
-                        .contentType(ContentType.JSON)
-                        .when().post("/themes")
-                        .then().log().all().extract();
+            void given_테마(){
+                given_테마_삭제(this.given_테마_id);
+                SaveThemeRequest given_테마 = new SaveThemeRequest("테마","테마설명","썸네일 URL");
+                ReadThemeResponse theme = themeService.saveTheme(given_테마);
+
+                this.given_테마_id = theme.id();
             }
 
             @Test
             @DisplayName("삭제하고, void를 리턴합니다.")
             void it_return_void_and_not_delete_theme(){
-                var response = RestAssured
-                        .given().log().all()
-                        .contentType(ContentType.JSON)
-                        .when().delete("/themes/1")
-                        .then().log().all().extract();
+                given_테마();
+                given_테마_삭제(this.given_테마_id);
 
-                assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+                assertThatThrownBy(() ->  themeService.readThemeById(this.given_테마_id))
+                        .isInstanceOf(EmptyResultDataAccessException.class);
             }
         }
     }

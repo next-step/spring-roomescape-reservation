16934078511decee89c0489b5fdc111067278979
yeeuniii <anna862700@gmail.com>
2024-06-14 02:18:40 +0900

    test(step2): 중복 예약 테스트 코드 추가

--- src/test/java/roomescape/ReservationTest.java
+++ src/test/java/roomescape/ReservationTest.java
@@ -4,26 +4,44 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.DTO.ReservationRequest;
 import roomescape.DTO.ReservationResponse;
 import roomescape.DTO.ReservationTimeRequest;
-import roomescape.DTO.ReservationTimeResponse;
+import roomescape.DTO.ThemeRequest;
+import roomescape.Service.ReservationService;
+import roomescape.Service.ReservationTimeService;
+import roomescape.Service.ThemeService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ReservationTest {
+    @Autowired
+    private ReservationService reservationService;
+    @Autowired
+    private ReservationTimeService reservationTimeService;
+
+    @Autowired
+    private ThemeService themeService;
+
+    private void set() {
+        reservationTimeService.add(new ReservationTimeRequest("13:00"));
+        reservationTimeService.add(new ReservationTimeRequest("15:00"));
+        themeService.add(new ThemeRequest("theme1", "bla", ""));
+        themeService.add(new ThemeRequest("theme2", "fun", ""));
+    }
+
     @Test
     @DisplayName("ReservationController - create()")
     void 예약() {
         String name = "yeeun";
         String date = "2024-12-13";
+
         new ReservationTimeTest().예약_시간_생성();
         new ThemeTest().테마_생성();
 
@@ -93,6 +111,23 @@ public class ReservationTest {
     }
 
     @Test
+    @DisplayName("ReservationController - create() duplicated date, time and theme")
+    void 중복_예약() {
+        String date = "2024-12-13";
+        set();
+        reservationService.make(new ReservationRequest("yeeun", date, 1L, 1L));
+
+        var response = RestAssured
+                .given().log().all()
+                .body(new ReservationRequest("brown", date, 1L, 1L))
+                .contentType(ContentType.JSON)
+                .when().post("/reservations")
+                .then().log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
     @DisplayName("ReservationController - read()")
     void 젼체_예약_조회() {
         예약();
@@ -118,7 +153,7 @@ public class ReservationTest {
     }
 
     @Test
-    @DisplayName("ReservationController - delete() 1")
+    @DisplayName("ReservationController - delete()")
     void 예약_취소() {
         예약();
 
@@ -131,7 +166,7 @@ public class ReservationTest {
     }
 
     @Test
-    @DisplayName("ReservationController - delete() 2")
+    @DisplayName("ReservationController - delete() : not existent reservation")
     void 존재하지_않는_예약_취소() {
         var response = RestAssured
                 .given().log().all()

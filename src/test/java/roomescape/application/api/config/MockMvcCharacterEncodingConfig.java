5827c924ac218a8d0078069bcd19a40e253d54eb
kilian <audrb96@gmail.com>
2024-06-14 17:21:15 +0900

    test : Theme 생성 API 컨트롤러 테스트

---
+++ src/test/java/roomescape/application/api/config/MockMvcCharacterEncodingConfig.java
@@ -0,0 +1,14 @@
+package roomescape.application.api.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@TestConfiguration
+public class MockMvcCharacterEncodingConfig {
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        return new CharacterEncodingFilter("UTF-8", true);
+    }
+}

ed91ea0e456f29fcd6a2d197380a27b64d907e2a
yeeuniii <anna862700@gmail.com>
2024-06-19 01:35:21 +0900

    refactor(step2): 피드백 반영 - exception handler 수정

--- src/main/java/roomescape/Exception/application/GlobalExceptionHandler.java
+++ src/main/java/roomescape/Exception/application/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.exception.dto.ExceptionResponse;
 import roomescape.exception.BadRequestException;
@@ -12,16 +13,14 @@ import roomescape.exception.NotFoundException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException exception) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(ExceptionResponse.createByMessage(exception.getMessage()));
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public ExceptionResponse handleNotFoundException(NotFoundException exception) {
+        return ExceptionResponse.createByMessage(exception.getMessage());
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ExceptionResponse> handleBadRequestException(Exception exception) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(ExceptionResponse.createByMessage(exception.getMessage()));
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleBadRequestException(Exception exception) {
+        return ExceptionResponse.createByMessage(exception.getMessage());
     }
 }

63dbae7872decc5671593c5e5dfd2cffb3f8f777
동원 <ai06021@gmail.com>
2024-06-22 04:25:08 +0900

    feat : step1 reserve manage

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,9 +1,18 @@
 package controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import domain.Reserve;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
+    private List<Reserve> reserves = new ArrayList<>();
+    private Long id = 0L;
 
     @GetMapping("/")
     public String home() {
@@ -13,5 +22,23 @@ public class Controller {
     @GetMapping("/admin/reservation")
     public String adminReservation() {
         return "admin/reservation-legacy";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reserve>> readReserve() {
+        return ResponseEntity.ok().body(reserves);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reserve> addReserve(@RequestBody Reserve reserve) {
+        reserve.setId(id++);
+        reserves.add(reserve);
+        return ResponseEntity.created(URI.create("api/reserve/" + reserve.getId())).body(reserve);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReserve(@PathVariable Long id) {
+        reserves.remove(id);
+        return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/domain/Reserve.java
@@ -0,0 +1,35 @@
+package domain;
+
+public class Reserve {
+    long id;
+    String name;
+    String date;
+    String time;
+
+    public Reserve(long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}

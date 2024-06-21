04238093d64aa86339381199a3f930beeb440882
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-21 16:18:16 +0900

    FEAT : PageController 생성 - home과 reservation 페이지로 이동 가능

---
+++ src/main/java/roomescape/controller/PageController.java
@@ -0,0 +1,17 @@
+package roomescape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping
+    public String home() {
+        return "admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String reservation() {
+        return "admin/reservation-legacy";
+    }
+}
---
+++ src/main/java/roomescape/model/Reservation.java
@@ -0,0 +1,53 @@
+package roomescape.repository;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    private String date;
+    private String time;
+
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static Reservation toEntity(Reservation reservation, AtomicLong id) {
+        return new Reservation(Long.valueOf(String.valueOf(id)), reservation.getName(), reservation.getDate(), reservation.getTime());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}

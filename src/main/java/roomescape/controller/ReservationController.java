ee2eb721300fb2c6efb332dcc79193981b9b1925
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-21 16:19:51 +0900

    FEAT : ReservationController 생성 - 예약 추가, 조회, 삭제 API 작성

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,2 +1,38 @@
-package roomescape.controller;public class ReservationController {
+package roomescape.controller;
+
+import org.springframework.web.bind.annotation.*;
+import roomescape.model.Reservation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+    private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
+
+    @PostMapping
+    public Reservation create(@RequestBody Reservation reservation) {
+        Reservation newReservation = Reservation.toEntity(reservation, index);
+        reservations.add(newReservation);
+        return newReservation;
+    }
+
+    @GetMapping
+    public List<Reservation> read() {
+        return reservations;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(findReservation -> Objects.equals(findReservation.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+
+        reservations.remove(reservation);
+    }
 }

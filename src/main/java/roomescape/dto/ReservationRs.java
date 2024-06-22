8a2b2d18d2962d5feb434326ec313808cfbd9db8
koreacat <kj9966@naver.com>
2024-06-22 21:02:23 +0900

    fix: 1단계 - 예약 기능 구현 > 리뷰 반영

---
+++ src/main/java/roomescape/dto/ReservationRs.java
@@ -0,0 +1,51 @@
+package roomescape.dto;
+
+import roomescape.model.ReservationTime;
+
+public class ReservationRs {
+    private Long id;
+    private String name;
+    private String date;
+    private ReservationTime time;
+
+    public ReservationRs() {}
+
+    public ReservationRs(Long id, String name, String date, ReservationTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public ReservationTime getTime() {
+        return time;
+    }
+
+    public void setTime(ReservationTime time) {
+        this.time = time;
+    }
+}

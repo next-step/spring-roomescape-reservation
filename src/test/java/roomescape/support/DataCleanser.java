82c182c067142c84e9665c9a5d46caf93fa08af9
haero77 <shineecard5@gmail.com>
2024-06-23 18:35:59 +0900

    test: 데이터 클렌징 시 예약 시간 데이터도 같이 지우도록 수정

--- src/test/java/roomescape/support/DataCleanser.java
+++ src/test/java/roomescape/support/DataCleanser.java
@@ -3,14 +3,19 @@ package roomescape.support;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import roomescape.domain.reservation.repository.ReservationRepository;
+import roomescape.domain.reservationtime.repository.ReservationTimeRepository;
 
 @Component
 public class DataCleanser {
 
     @Autowired
+    ReservationTimeRepository reservationTimeRepository;
+
+    @Autowired
     ReservationRepository reservationRepository;
 
     public void clean() {
+        reservationTimeRepository.deleteAllInBatch();
         reservationRepository.deleteAllInBatch();
     }
 }

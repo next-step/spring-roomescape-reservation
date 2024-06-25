9ce9565696672e51fa8b440f30c7bbcf5ec5795e
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-25 21:13:13 +0900

    FEAT : ReservationTime 생성 - id와 startAt을 멤버로 가짐

--- src/main/java/roomescape/model/ReservationTime.java
+++ src/main/java/roomescape/model/ReservationTime.java
@@ -1,2 +1,24 @@
-package roomescape.model;public class ReservationTime {
+package roomescape.model;
+
+public class ReservationTime {
+    private Long id;
+    private String startAt;
+
+    public ReservationTime(String startAt) {
+        this(0L, startAt);
+    }
+
+    public ReservationTime(Long id, String startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStartAt() {
+        return startAt;
+    }
+
 }

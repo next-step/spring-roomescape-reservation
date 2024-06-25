2a95a4f306d86a43be5ccad74c86f0c638fd7910
jsween5723 <jsween5723@gmail.com>
2024-06-26 08:00:58 +0900

    refactor(reservation): 불필요 코드 제거

--- src/main/java/roomescape/domain/reservation/CreateReservation.java
+++ src/main/java/roomescape/domain/reservation/CreateReservation.java
@@ -1,11 +1,7 @@
 package roomescape.domain.reservation;
 
 import java.time.LocalDate;
-import roomescape.domain.theme.Theme;
-import roomescape.domain.time.ReservationTime;
 
 public record CreateReservation(String name, LocalDate date, long timeId, long themeId) {
-  public Reservation toReservation(long id) {
-    return new Reservation(id, name, date, new ReservationTime(timeId), new Theme(themeId));
-  }
+
 }

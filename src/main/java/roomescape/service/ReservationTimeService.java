0d477e926fbd7cd56f64c7f62c743b0827c226ea
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-25 21:14:13 +0900

    FEAT : ReservationTimeService 생성

--- src/main/java/roomescape/service/ReservationTimeService.java
+++ src/main/java/roomescape/service/ReservationTimeService.java
@@ -1,2 +1,32 @@
-package roomescape.controller;public class ReservationTimeService {
+package roomescape.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import roomescape.model.ReservationTime;
+import roomescape.repository.ReservationRepository;
+import roomescape.repository.ReservationTimeRepository;
+
+import java.util.List;
+
+@Service
+public class ReservationTimeService {
+
+    private final ReservationTimeRepository reservationTimeRepository;
+
+    @Autowired
+    public ReservationTimeService(ReservationTimeRepository reservationTimeRepository) {
+        this.reservationTimeRepository = reservationTimeRepository;
+    }
+
+    public ReservationTime addReservation(String startAt) {
+        return reservationTimeRepository.save(startAt);
+    }
+
+    public List<ReservationTime> lookUpReservationTime() {
+        return reservationTimeRepository.readAll();
+    }
+
+    public void deleteReservation(Long id) {
+        reservationTimeRepository.deleteById(id);
+    }
 }

f4993f662f56930d83fec020bbd2765b2dd3a27c
jsween5723 <jsween5723@gmail.com>
2024-06-26 08:46:12 +0900

    refactor(time): 불필요 코드 제거

--- src/main/java/roomescape/domain/theme/CreateTheme.java
+++ src/main/java/roomescape/domain/theme/CreateTheme.java
@@ -1,7 +1,4 @@
 package roomescape.domain.theme;
 
 public record CreateTheme(String name, String description, String thumbnail) {
-  public Theme toDomain(long id) {
-    return new Theme(id, name, description, thumbnail);
-  }
 }

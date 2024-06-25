c7eb7224d401cf31ac6dcee61dc3860a0a5d9136
haero77 <shineecard5@gmail.com>
2024-06-25 09:27:07 +0900

    refactor: 클래스명 변경 (명확성 향상)

--- src/main/java/roomescape/admin/controller/AdminPageController.java
+++ src/main/java/roomescape/admin/controller/AdminPageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdminController {
+public class AdminPageController {
 
     @GetMapping("/")
     public String home() {
@@ -15,5 +15,4 @@ public class AdminController {
     public String reservation() {
         return "admin/reservation-legacy";
     }
-
 }

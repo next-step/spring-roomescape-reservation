ff82e42cdb211ab86edb744433960e7e04b7e063
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-26 22:30:58 +0900

    CHORE : 필요없는 import문 삭제

--- src/main/java/roomescape/model/ReservationTime.java
+++ src/main/java/roomescape/model/ReservationTime.java
@@ -4,10 +4,6 @@ public class ReservationTime {
     private Long id;
     private String startAt;
 
-    public ReservationTime(String startAt) {
-        this(0L, startAt);
-    }
-
     public ReservationTime(Long id, String startAt) {
         this.id = id;
         this.startAt = startAt;

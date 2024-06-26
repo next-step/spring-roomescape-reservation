ccc3c0cdf6576d2655e851582b627e306c594744
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-26 22:26:57 +0900

    FEAT : ThemeRepository 생성

--- src/main/java/roomescape/repository/ThemeRepository.java
+++ src/main/java/roomescape/repository/ThemeRepository.java
@@ -1,2 +1,53 @@
-package roomescape.service;public class ThemeRepository {
+package roomescape.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import roomescape.model.Theme;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ThemeRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert jdbcInsert;
+    private final RowMapper rowMapper = (resultSet, rowNum) -> new Theme(
+        resultSet.getLong("id"),
+        resultSet.getString("name"),
+        resultSet.getString("description"),
+        resultSet.getString("thumbnail")
+    );
+
+    @Autowired
+    public ThemeRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("theme")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    public Long save(Theme theme) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", theme.getName());
+        params.put("description", theme.getDescription());
+        params.put("thumbnail", theme.getThumbnail());
+
+        return jdbcInsert.executeAndReturnKey(params).longValue();
+    }
+
+    public List<Theme> readAll() {
+        String sql = "SELECT * FROM theme";
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public void deleteById(Long id) {
+        String sql = "DELETE FROM theme WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
 }

d834d7f387b8fd546b472b25121f120f66eff0e4
gjtmdwns <gjtmdwns0815@naver.com>
2024-06-26 22:26:34 +0900

    FEAT : Theme 생성

--- src/main/java/roomescape/model/Theme.java
+++ src/main/java/roomescape/model/Theme.java
@@ -1,2 +1,31 @@
-package roomescape.model;public class Theme {
+package roomescape.model;
+
+public class Theme {
+    private Long id;
+    private String name;
+    private String description;
+    private String thumbnail;
+
+    public Theme(Long id, String name, String description, String thumbnail) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
 }

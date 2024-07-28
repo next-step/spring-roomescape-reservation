dd901987a91a7adb508dc789e7637fac45afec37
haero77 <shineecard5@gmail.com>
2024-07-28 18:49:52 +0900

    migration: 도메인 모듈 분리

--- src/main/java/roomescape/domain/reservationtime/exception/ReservationTimeAlreadyInUse.java
+++
@@ -1,8 +0,0 @@
-package roomescape.domain.reservationtime.exception;
-
-public class ReservationTimeAlreadyInUse extends ReservationTimeException{
-
-    public ReservationTimeAlreadyInUse(final String message) {
-        super(message);
-    }
-}

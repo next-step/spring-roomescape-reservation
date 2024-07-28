5a7bad0a52a6b55d571f8754408eef7c4a906582
haero77 <shineecard5@gmail.com>
2024-07-29 00:28:52 +0900

    migration: web 모듈 분리

--- web/src/main/java/roomescape/web/admin/controller/AdminPageController.java
+++ web/src/main/java/roomescape/web/admin/controller/AdminPageController.java
@@ -1,4 +1,4 @@
-package roomescape.admin.controller;
+package roomescape.web.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

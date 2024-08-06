64dca821f94e2a964c62d05d7be4536c264c8663
haero77 <shineecard5@gmail.com>
2024-08-06 21:26:10 +0900

    test: 데이터 클렌징을 위한 테마 엔티티 전체 삭제 기능 구현

--- storage/db-core/src/test/java/roomescape/db/core/DataCleanser.java
+++ storage/db-core/src/test/java/roomescape/db/core/DataCleanser.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import roomescape.db.core.reservation.ReservationJdbcRepository;
 import roomescape.db.core.reservatiotime.ReservationTimeJdbcRepository;
+import roomescape.db.core.theme.ThemeJdbcRepository;
 
 @Component
 public class DataCleanser {
@@ -14,8 +15,12 @@ public class DataCleanser {
     @Autowired
     ReservationJdbcRepository reservationRepository;
 
+    @Autowired
+    ThemeJdbcRepository themeJdbcRepository;
+
     public void clean() {
         reservationRepository.deleteAllInBatch();
         reservationTimeRepository.deleteAllInBatch();
+        themeJdbcRepository.deleteAllInBatch();
     }
 }

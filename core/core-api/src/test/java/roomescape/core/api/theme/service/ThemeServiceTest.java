71ddbf1271900cf12ec664c43d035f7832a0ae66
haero77 <shineecard5@gmail.com>
2024-08-16 12:57:50 +0900

    feat: 테마 삭제 API 추가

--- core/core-api/src/test/java/roomescape/core/api/theme/service/ThemeServiceTest.java
+++ core/core-api/src/test/java/roomescape/core/api/theme/service/ThemeServiceTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import roomescape.core.api.support.IntegrationTestSupport;
 import roomescape.core.api.theme.service.request.ThemeAppendRequest;
+import roomescape.core.domain.common.ActiveStatus;
 import roomescape.core.domain.theme.Theme;
+import roomescape.core.domain.theme.ThemeId;
 import roomescape.core.domain.theme.ThemeRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,7 +39,38 @@ class ThemeServiceTest extends IntegrationTestSupport {
                 () -> assertThat(actual.getId()).isNotNull(),
                 () -> assertThat(actual.getName()).isEqualTo("name"),
                 () -> assertThat(actual.getDescription()).isEqualTo("description"),
-                () -> assertThat(actual.getThumbnail()).isEqualTo("https://thumbnail.com")
+                () -> assertThat(actual.getThumbnail()).isEqualTo("https://thumbnail.com"),
+                () -> assertThat(actual.getActiveStatus()).isEqualTo(ActiveStatus.ACTIVE)
         );
+    }
+
+    @DisplayName("id로 테마를 삭제할 수 있다")
+    @Test
+    void deleteTheme() {
+        // given
+        final Theme themeToDelete = saveTheme("name1", "description1", "https://thumbnail.com1");
+        final ThemeId themeId = themeToDelete.getId();
+
+        // when
+        sut.deleteTheme(themeId);
+
+        // then
+        final Theme actual = themeRepository.findById(themeId).get();
+        assertAll(
+                () -> assertThat(actual.getName()).isEqualTo("name1"),
+                () -> assertThat(actual.getDescription()).isEqualTo("description1"),
+                () -> assertThat(actual.getThumbnail()).isEqualTo("https://thumbnail.com1"),
+                () -> assertThat(actual.getActiveStatus()).isEqualTo(ActiveStatus.DELETED)
+        );
+    }
+
+    private Theme saveTheme(String name, String description, String thumbnail) {
+        final Theme theme = Theme.builder()
+                .name(name)
+                .description(description)
+                .thumbnail(thumbnail)
+                .activeStatus(ActiveStatus.ACTIVE)
+                .build();
+        return themeRepository.save(theme);
     }
 }

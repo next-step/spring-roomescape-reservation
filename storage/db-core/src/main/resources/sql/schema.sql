a2a1d5d0f5968b8cf7d05a239529d4992ce11f66
haero77 <shineecard5@gmail.com>
2024-08-20 21:27:19 +0900

    docs: schema.sql - 테마 테이블 추가

--- storage/db-core/src/main/resources/sql/schema.sql
+++ storage/db-core/src/main/resources/sql/schema.sql
@@ -1,3 +1,18 @@
+DROP TABLE IF EXISTS reservations;
+create table reservations
+(
+    reservation_id bigint       not null auto_increment,
+    theme_id       bigint       not null,
+    name           varchar(255) not null,
+    date           varchar(255) not null,
+    time_id        bigint       not null,
+    active_status  varchar(255) not null default 'ACTIVE',
+    deleted_at     varchar(255),
+    created_at     varchar(255) not null,
+    primary key (reservation_id)
+);
+
+DROP TABLE IF EXISTS reservation_times;
 create table reservation_times
 (
     time_id    bigint       not null auto_increment,
@@ -6,23 +21,13 @@ create table reservation_times
     primary key (time_id)
 );
 
-create table reservations
-(
-    reservation_id bigint       not null auto_increment,
-    name           varchar(255) not null,
-    date           varchar(255) not null,
-    time_id        bigint       not null,
-    status         varchar(255) not null default 'CONFIRMED',
-    canceled_at    varchar(255),
-    created_at     varchar(255) not null,
-    primary key (reservation_id)
-);
-
+DROP TABLE IF EXISTS themes;
 CREATE TABLE themes
 (
-    theme_id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    thumbnail   VARCHAR(255) NOT NULL,
+    theme_id      BIGINT       NOT NULL AUTO_INCREMENT,
+    name          VARCHAR(255) NOT NULL,
+    description   VARCHAR(255) NOT NULL,
+    thumbnail     VARCHAR(255) NOT NULL,
+    active_status VARCHAR(255) NOT NULL DEFAULT 'ACTIVE',
     PRIMARY KEY (theme_id)
 );

0ae7f0c2259a2e47762821ede59c8902eb14996e
haero77 <shineecard5@gmail.com>
2024-10-03 23:22:24 +0900

    refactor: 사용하지 않는 커스텀 예외 삭제

--- core/core-domain/src/main/java/roomescape/core/domain/common/exception/DomainException.java
+++
@@ -1,8 +0,0 @@
-package roomescape.core.domain.common.exception;
-
-public class DomainException extends RuntimeException {
-
-    public DomainException(final String message) {
-        super(message);
-    }
-}

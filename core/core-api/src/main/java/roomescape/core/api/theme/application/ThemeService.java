0a4a24034164faa6022dde9535b48fcfe5732eb2
haero77 <shineecard5@gmail.com>
2024-10-04 00:18:25 +0900

    feat: 테마 삭제 시 해당 테마로 예약된 예약이 있으면 예약 삭제를 불가능하도록 수정

--- core/core-api/src/main/java/roomescape/core/api/theme/application/ThemeService.java
+++ core/core-api/src/main/java/roomescape/core/api/theme/application/ThemeService.java
@@ -2,10 +2,14 @@ package roomescape.core.api.theme.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import roomescape.core.api.theme.application.request.ThemeAppendRequest;
+import roomescape.core.domain.reservation.Reservation;
+import roomescape.core.domain.reservation.ReservationRepository;
 import roomescape.core.domain.theme.Theme;
 import roomescape.core.domain.theme.ThemeId;
 import roomescape.core.domain.theme.ThemeRepository;
+import roomescape.core.domain.theme.exception.ThemeAlreadyInUseException;
 
 import java.util.List;
 
@@ -14,6 +18,7 @@ import java.util.List;
 public class ThemeService {
 
     private final ThemeRepository themeRepository;
+    private final ReservationRepository reservationRepository;
 
     public List<Theme> findAll() {
         return themeRepository.findNotDeletedThemes();
@@ -24,8 +29,17 @@ public class ThemeService {
     }
 
     public void deleteTheme(final ThemeId themeId) {
+        verifyThemeNotInUse(themeId);
+
         final Theme theme = themeRepository.getById(themeId);
         final Theme deleted = theme.delete();
         themeRepository.save(deleted);
+    }
+
+    private void verifyThemeNotInUse(final ThemeId themeId) {
+        final List<Reservation> reservations = reservationRepository.findAllByThemeId(themeId);
+        if (!CollectionUtils.isEmpty(reservations)) {
+            throw ThemeAlreadyInUseException.from(themeId);
+        }
     }
 }
